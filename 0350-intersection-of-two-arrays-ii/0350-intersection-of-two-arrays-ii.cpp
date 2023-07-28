c5e72460810d717e58dd9fbb276ae8ca308a12cb
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-07-28 23:12:51 +0530

    Time: 3 ms (91.77%), Space: 10.1 MB (70.87%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        //two pointer 
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        vector<int>ans;  //changes occur here 
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(nums1[i]>nums2[j]) j++;
+            else if(nums1[i]<nums2[j]) i++;
+            else{
+                ans.push_back(nums1[i++]);
+                j++;
+            }
+        }
+        return ans;
+    }
+};
