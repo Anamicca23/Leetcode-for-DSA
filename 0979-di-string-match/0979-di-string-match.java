7d3cb36bd14677fc3a41c16fe207f799f8d3954f
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-10-19 11:14:51 +0530

    Time: 3 ms (62.65%), Space: 44.7 MB (79.87%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int low=0;
+        int high=s.length();
+        int ans[]=new int[s.length()+1];
+     for(int i=0;i<s.length();i++){
+         if(s.charAt(i)=='I'){
+             ans[i]=low;
+             low++;
+         }
+         else{
+             ans[i]=high;
+             high--;
+         }
+     }
+     ans[s.length()]=high;
+return ans;
+    }
+}
