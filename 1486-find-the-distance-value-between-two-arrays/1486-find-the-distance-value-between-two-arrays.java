f7d0c119dd2a682aaa851b6a314e9c053adc7ee8
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-10-21 16:22:08 +0530

    Time: 3 ms (98.41%), Space: 42.9 MB (76.09%) - LeetHub

---
+++ 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        Arrays.sort(arr2);    
+        int distance = 0;
+        
+        for (int val : arr1) {
+            if (notInRange(arr2, val - d, val + d)) {
+                distance++;
+            }
+        }    
+        
+        return distance;
+    }
+    
+    // Checks if the array doesn't contain any value in range (from <= value <= to) using binary search
+    private static boolean notInRange(int[] arr, int from, int to) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] >= from && arr[mid] <= to) {
+                return false;
+            } else if (arr[mid] < from) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return true;
+    }
+}
