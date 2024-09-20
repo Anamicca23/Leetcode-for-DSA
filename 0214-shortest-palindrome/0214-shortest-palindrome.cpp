9a3d2f2565e4e25d343be50c4fa261e2a0defe53
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-09-20 23:15:50 +0530

    Time: 13 ms (13.18%), Space: 12 MB (17.91%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    string shortestPalindrome(string s) {
-        int n = s.size();
-        string ss = s + '#' + string(s.rbegin(),s.rend());
-        vector<int> lps(ss.size());
-
-        for(int i=1; i<ss.size(); i++){
-            int j = lps[i-1];
-
-            while(j>0 && ss[i] != ss[j]){
-                j = lps[j-1];
+    vector<int>z_function(string s){
+        int n=s.length();
+        vector<int>z(n,0);
+        int l=0,r=0;
+        for(int i=1;i<n;i++){
+            if(r>i){
+                z[i]=min(z[i-l],r-i);
             }
-
-            if(ss[i] == ss[j]){
-                j++;
+            while(i+z[i]<n && s[i+z[i]]==s[z[i]])z[i]++;
+            if(i+z[i]>r){
+                l=i;
+                r=i+z[i];
             }
-
-            lps[i] = j;
         }
-
-        return string(s.rbegin(),s.rend()).substr(0,n-lps.back()) + s;
+        return z;
+    }
+    string shortestPalindrome(string s) {
+        int n=s.length();
+        string t=s;
+        reverse(t.begin(),t.end());
+        t=s+"#"+t;
+        vector<int>z=z_function(t);
+        int mxpal=0;
+        for(int i=0;i<n;i++){
+            if(z[2*n-i]==i+1){
+                mxpal=i+1;
+            }
+        }
+        string ans=s.substr(mxpal,n-mxpal);
+        reverse(ans.begin(),ans.end());
+        return ans+s;
     }
 };
