e2206fb88d8e2e5b0ec9ab5a09b710e54b959988
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-09-21 11:41:36 +0530

    Time: 8 ms (73.38%), Space: 12.5 MB (91.06%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for (int num = 1; ans.size() < n;) {
+            ans.push_back(num);
+            if (num * 10 <= n) num *= 10;
+            else {
+                if (num % 10 == 9 || num + 1 > n) while ((num /= 10) % 10 == 9);
+                num++;
+            }
+        }
+        return ans;
+    }
+};
