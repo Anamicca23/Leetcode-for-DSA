bd53205a1b75e2a19aebf1fc4c2b1733477d67c9
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-09-27 15:50:32 +0530

    Time: 152 ms (99.61%), Space: 17.5 MB (45.71%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,28 +1,61 @@
 class MyCalendarTwo:
+
     def __init__(self):
-        self.cal = []  
-        self.overlap = []  
+        self.single_booked = []
+        self.double_booked = []
+        
+    def intersection(self, intervals, s, e):
 
-    def overlapped(self, start1, end1, start2, end2):
-        return max(start1, start2) < min(end1, end2)
+        l = bisect.bisect_left(intervals, s)
+        r = bisect.bisect_right(intervals, e)
+        
+        intersection = []
+        
+        if l % 2:
+            # we may create empty interval where s == intervals[l]
+            if intervals[l] != s:
+                intersection.append(s)
+            else:
+                l = l + 1
 
-    def overlapped_interval(self, start1, end1, start2, end2):
-        return (max(start1, start2), min(end1, end2))
+        intersection += intervals[l:r]
 
-    def book(self, start, end):
-        for book in self.overlap:
-            if self.overlapped(book[0], book[1], start, end):
-                return False  
+        if r % 2:
+            # we may create empty interval where e == intervals[r-1]
+            if intervals[r-1] != e:
+                intersection.append(e)
+            else:
+                intersection.pop()
 
+        return intersection
+        
+    def add(self, intervals, s, e):
 
-        for book in self.cal:
-            if self.overlapped(book[0], book[1], start, end):
-                self.overlap.append(self.overlapped_interval(book[0], book[1], start, end))
+        l = bisect.bisect_left(intervals, s)
+        r = bisect.bisect_right(intervals, e)
+        
+        new = []
+        if not l % 2:
+            new.append(s)
+            
+        if not r % 2:
+            new.append(e)
 
+        intervals[l:r] = new
 
-        self.cal.append((start, end))
-        return True 
+    def book(self, start: int, end: int) -> bool:
 
-# Example usage:
-# obj = MyCalendarTwo()
-# param_1 = obj.book(start, end)
+        if self.intersection(self.double_booked, start, end):
+            return False
+        
+        intersection = self.intersection(self.single_booked, start, end)
+
+        if intersection:
+            for i in range(len(intersection) // 2):
+                i1 = intersection[2*i]
+                i2 = intersection[2*i+1]
+                self.add(self.double_booked, i1, i2)
+
+        self.add(self.single_booked, start, end)
+
+        return True
