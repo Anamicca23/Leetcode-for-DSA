39f84c45f553407f4744dd8d605d365b1c8cc38b
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-09-28 23:00:00 +0530

    Time: 22 ms (40.56%), Space: 23.1 MB (11.65%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.cpp
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -1,3 +1,9 @@
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("-ffloat-store")
+#pragma GCC optimize ("O3", "unroll-loops")
+auto _=[]()noexcept{ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
+
 class Node{
 public:
     int data;
