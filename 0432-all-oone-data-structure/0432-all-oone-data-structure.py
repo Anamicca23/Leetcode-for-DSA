8ce6fb78b303824866e33766413ad934d0345620
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-09-29 18:07:45 +0530

    Time: 139 ms (94.75%), Space: 31.9 MB (94.21%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,57 +1,68 @@
 class AllOne:
 
     def __init__(self):
-        self.count = {}  # Stores the count of each key
-        self.key_map = {}  # Maps count to keys with that count
-    
+       
+        self.d={}
+       
+        self.op=0
+       
+        self.res=""
+
     def inc(self, key: str) -> None:
-        # Get the current count of the key
-        current_count = self.count.get(key, 0)
-        
-        # Remove key from current count mapping (if it exists)
-        if current_count > 0:
-            self.key_map[current_count].remove(key)
-            if not self.key_map[current_count]:
-                del self.key_map[current_count]
-        
-        # Increment the count of the key
-        new_count = current_count + 1
-        self.count[key] = new_count
-        
-        # Add the key to the new count mapping
-        if new_count not in self.key_map:
-            self.key_map[new_count] = set()
-        self.key_map[new_count].add(key)
-    
-    def dec(self, key: str) -> None:
-        # Get the current count of the key
-        current_count = self.count.get(key, 0)
-        if current_count == 0:
-            return
-        
-        # Remove key from the current count mapping
-        self.key_map[current_count].remove(key)
-        if not self.key_map[current_count]:
-            del self.key_map[current_count]
-        
-        # Decrease the count of the key
-        new_count = current_count - 1
-        if new_count > 0:
-            self.count[key] = new_count
-            if new_count not in self.key_map:
-                self.key_map[new_count] = set()
-            self.key_map[new_count].add(key)
+      
+        self.op=0
+        if (key in self.d):
+            self.d[key]+=1
         else:
-            del self.count[key]
-    
+            self.d[key]=1
+        return None
+    def dec(self, key: str) -> None:
+     
+        self.op=0
+        if (self.d[key]>1):
+            self.d[key]-=1
+        else:
+            del self.d[key]
+        return None
+
     def getMaxKey(self) -> str:
-        if not self.count:
-            return ""
-        max_count = max(self.key_map)
-        return next(iter(self.key_map[max_count]))
-    
+       
+        if (self.op==1):
+            return self.res
+        self.op=1
+        if (self.d):
+            a=list(self.d.values())[0]
+            b=list(self.d.keys())[0]
+            for i in self.d.keys():
+                if (self.d[i]>a):
+                    a=self.d[i]
+                    b=i
+            self.res=b
+            return b
+        self.res=""
+        return ""
+
     def getMinKey(self) -> str:
-        if not self.count:
-            return ""
-        min_count = min(self.key_map)
-        return next(iter(self.key_map[min_count]))
+       
+        if (self.op==2):
+            return self.res
+        self.op=2
+        if (self.d):
+            a=list(self.d.values())[0]
+            b=list(self.d.keys())[0]
+            for i in self.d.keys():
+                if (self.d[i]<a):
+                    a=self.d[i]
+                    b=i
+            self.res=b
+            return b
+        self.res=""
+        return ""
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
