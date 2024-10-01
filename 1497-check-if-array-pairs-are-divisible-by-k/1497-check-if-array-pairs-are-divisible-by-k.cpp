34db9daaa9b5f1e298c048fe15cc5e250e9923a5
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-01 19:31:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int>hs(k,0);
+        for(int &it:arr){
+            int rem=(it%k+k)%k;
+            hs[rem]++;
+        }
+        if(hs[0]&1)return false;
+        for(int i=1;i<k/2;i++){
+            if(hs[k-i]!=hs[i])return false;
+        }
+        return true;
+    }
+};
