1dce0ad84efd3aace78da27b175cb184c11d5b1e
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-09 19:50:08 +0530

    Time: 0 ms (100%), Space: 41.4 MB (64.62%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,10 +1,20 @@
-class Solution {
-public:
-    int minAddToMakeValid(string s) {
-        int size=0,openb=0;
-        for(auto &c:s){
-           (c == '(') ? size++ : (size > 0 ? size-- : openb++);
+public class Solution {
+    public int minAddToMakeValid(String s) {
+        int size = 0;
+        int openb = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                size++;
+            } else {
+                if (size > 0) {
+                    size--;
+                } else {
+                    openb++;
+                }
+            }
         }
-        return openb+size;
+
+        return openb + size;
     }
-};
+}
