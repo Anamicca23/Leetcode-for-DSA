588e7aee9b1caccbbf49c143022718aad28ef1c0
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-11 22:24:54 +0530

    Time: 30 ms (99.42%), Space: 48.4 MB (84.8%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,33 +1,85 @@
-import java.util.*;
-
 class Solution {
+
     public int smallestChair(int[][] times, int targetFriend) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        PriorityQueue<Integer> chairs = new PriorityQueue<>();
+        PriorityQueue<Node2> busyChair = new PriorityQueue<>();
+        PriorityQueue<Integer> emptyChair = new PriorityQueue<>();
         
-        int targetArrival = times[targetFriend][0];
-        Arrays.sort(times, Comparator.comparingInt(a -> a[0]));
-
-        int nextChair = 0, seat = 0;
-
-        for (int[] time : times) {
-            int arrival = time[0], leave = time[1];
-
-            // Free up chairs of friends who have already left
-            while (!pq.isEmpty() && pq.peek()[0] <= arrival) {
-                chairs.offer(pq.poll()[1]);
+        int n = times.length;
+        
+        Node[] arrDepNodeArray = new Node[n];
+        for(int i = 0; i < n; i++){
+            int[] time = times[i];
+            int arr = time[0];
+            int dep = time[1];
+            
+            Node arrDepNode = new Node(arr, dep);
+            arrDepNodeArray[i] = arrDepNode;
+            if(i == targetFriend){
+                arrDepNode.target = true;
             }
-
-            // Assign the smallest available chair
-            if (!chairs.isEmpty()) seat = chairs.poll();
-            else seat = nextChair++;
-
-            pq.offer(new int[] {leave, seat});
-
-            // If this is the target friend, return their seat
-            if (arrival == targetArrival) break;
         }
+        
+        Arrays.sort(arrDepNodeArray);
+        int chairNo = 1;
+        for(Node nodeIter : arrDepNodeArray){
+            int arr = nodeIter.arrival;
+            int dep = nodeIter.departure;
+            int chair = -1;
+            boolean target = nodeIter.target;
+            if(busyChair.size() == 0){
+                chair = 0;
+                busyChair.add(new Node2(dep, chair));
+            }else{
+                while(busyChair.size() > 0 && busyChair.peek().departure <= arr){
+                    Node2 removeNode = busyChair.poll();
+                    emptyChair.add(removeNode.chair);
+                }
+                if(emptyChair.size() > 0){
+                    chair = emptyChair.poll();   
+                }else{
+                    chair = chairNo;
+                    chairNo++;
+                }
+                busyChair.add(new Node2(dep, chair));
+            }
+            
+            if(target){
+                return chair;
+            }
+        }
+        return -1;
+    }
+}
 
-        return seat;
+
+class Node implements Comparable<Node>{
+
+    int arrival;
+    int departure;
+    boolean target = false;
+    
+    public Node(int arrival, int departure){
+        this.arrival = arrival;
+        this.departure = departure;
+    }
+    
+    public int compareTo(Node o){
+        return this.arrival - o.arrival;
+    }
+}
+
+class Node2 implements Comparable<Node2>{
+
+    int departure;
+    int chair;
+    
+    
+    public Node2(int departure, int chair){
+        this.chair = chair;
+        this.departure = departure;
+    }
+    
+    public int compareTo(Node2 o){
+        return this.departure - o.departure;
     }
 }
