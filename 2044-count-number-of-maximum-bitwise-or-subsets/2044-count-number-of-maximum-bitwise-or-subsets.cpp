7f6096a047a52b6fcd9588de24c8757dd7d3d101
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-18 16:27:40 +0530

    Time: 141 ms (11.86%), Space: 312.7 MB (6.63%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int subsetcount(int idx,int curror, vector<int>& nums,int maxor){
+    int subsetcount(int idx,int curror, vector<int>& nums,int maxor, vector<vector<int>> &dp){
         if(idx == nums.size()){
             if(curror==maxor){
                 return 1;
             }
             return 0;
         }
-        int included =  subsetcount(idx+1,curror | nums[idx],nums, maxor);
-        int notincluded = subsetcount(idx +1, curror, nums, maxor);
-        return included+notincluded;
+        if(dp[idx][curror]!=-1){
+            return dp[idx][curror];
+        }
+        int included =  subsetcount(idx+1,curror | nums[idx],nums, maxor,dp);
+        int notincluded = subsetcount(idx +1, curror, nums, maxor,dp);
+        return dp[idx][curror] = included+notincluded;
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int maxor=0;
         for(int &n:nums) maxor |= n;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(maxor+1,-1));
         int curror=0;
-        return subsetcount(0,curror,nums,maxor);
+        return  subsetcount(0,curror,nums,maxor,dp);
     }
 };
