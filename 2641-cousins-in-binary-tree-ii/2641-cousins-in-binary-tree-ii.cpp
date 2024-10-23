d433b5b0180b7b83cbb21bddc116374466d9100a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-23 21:28:05 +0530

    Time: 14 ms (97.55%), Space: 313 MB (29.84%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -1,31 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* replaceValueInTree(TreeNode* root) {
-        if (!root) return nullptr;
-        queue<TreeNode*> q;
-        int prev = root->val;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            int curr = 0;
-            while (size > 0) {
-                TreeNode* temp = q.front();
-                q.pop();
-                int leftRight = (temp->left ? temp->left->val : 0) + (temp->right ? temp->right->val : 0);
-                if (temp->left) {
-                    temp->left->val = leftRight;
-                    q.push(temp->left);
-                }
-                if (temp->right) {
-                    temp->right->val = leftRight;
-                    q.push(temp->right);
-                }
-                curr += leftRight;
-                temp->val = prev - temp->val;
-                size--;
-            }
-            prev = curr;
+    int sum[100001]={0}, h=0;
+    void dfs1(TreeNode*& root, int level){// compute level sum
+        if (!root) return ;
+        if (level>=h) h++;
+        sum[level]+=root->val;
+        dfs1(root->left, level+1);
+        dfs1(root->right, level+1);
+    }
+    void dfs2(TreeNode*& root, int level){
+        if (!root) return ;
+        if (level+1<=h){
+            int x=sum[level+1];
+            bool L=(root->left), R=(root->right);
+            x-=L?root->left->val:0;
+            x-=R?root->right->val:0;
+            if (L) root->left->val=x;// set childern's values
+            if (R) root->right->val=x;
         }
+        dfs2(root->left, level+1);
+        dfs2(root->right, level+1);
+    }
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        dfs1(root, 0);
+        root->val=0;
+        dfs2(root, 0);
         return root;
     }
 };
