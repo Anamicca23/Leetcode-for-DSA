e21ea565796fc410c66e66cc438cec98fc0bc53c
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-31 20:03:30 +0530

    Time: 22 ms (96.55%), Space: 10.9 MB (91.88%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    long long dp[100][100][100];
-    long long f(int i, int j, int k, vector<int>& robot, vector<vector<int>>& factory)
-    {
-        if (i<0) return 0;
-        if (j<0) return 1LL<<40;
-        if (dp[i][j][k]!=-1) return dp[i][j][k];
-        int xR=robot[i], xF=factory[j][0];
-        long long otherFactory=f(i, j-1, 0, robot, factory);
-        long long factoryJ=(k<factory[j][1])?abs(xR-xF)+f(i-1, j, k+1, robot, factory):1LL<<40;
-        return dp[i][j][k]=min(otherFactory,  factoryJ);
-
-    }
-    void print(auto& c){
-        for(int x:c) cout<<x<<", ";
-        cout<<endl;
-    }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        int n = robot.size();
+        int m = factory.size();
+        vector<long long> dp(n + 1, 10e12);
+        dp[0] = 0;
         sort(robot.begin(), robot.end());
         sort(factory.begin(), factory.end());
-        
-        int n=robot.size(), m=factory.size();
-        memset(dp, -1, sizeof(dp));
-        return f(n-1, m-1, 0, robot, factory);
+        for (int i = 0; i < m; i++) {
+            int position = factory[i][0];
+            int limit = factory[i][1];
+            while (limit--) {
+                for (int j = n - 1; j >= 0; j--) {
+                    dp[j + 1] = min(dp[j + 1], abs(robot[j] - position) + dp[j]);
+                }
+            }
+        }
+        return dp[n];
     }
 };
