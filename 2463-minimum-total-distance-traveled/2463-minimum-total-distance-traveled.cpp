84fec3a161b1e833d31d87e971f96376b4dab302
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-31 20:02:58 +0530

    Time: 68 ms (58.62%), Space: 19.3 MB (53.13%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
+    long long dp[100][100][100];
+    long long f(int i, int j, int k, vector<int>& robot, vector<vector<int>>& factory)
+    {
+        if (i<0) return 0;
+        if (j<0) return 1LL<<40;
+        if (dp[i][j][k]!=-1) return dp[i][j][k];
+        int xR=robot[i], xF=factory[j][0];
+        long long otherFactory=f(i, j-1, 0, robot, factory);
+        long long factoryJ=(k<factory[j][1])?abs(xR-xF)+f(i-1, j, k+1, robot, factory):1LL<<40;
+        return dp[i][j][k]=min(otherFactory,  factoryJ);
+
+    }
+    void print(auto& c){
+        for(int x:c) cout<<x<<", ";
+        cout<<endl;
+    }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
         sort(robot.begin(), robot.end());
         sort(factory.begin(), factory.end());
-        int m = robot.size(), n = factory.size();
-        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, LLONG_MAX));
-        dp[m][n] = 0;  // Set the base case for no robots left
-
-        for (int j = n - 1; j >= 0; j--) {
-            long long prefix = 0;
-            deque<pair<int, long long>> q = {{m, 0}};
-            for (int i = m - 1; i >= 0; i--) {
-                prefix += abs(robot[i] - factory[j][0]);
-                
-                while (!q.empty() && q.front().first > i + factory[j][1]) q.pop_front();
-                if (!q.empty() && q.front().second != LLONG_MAX)
-                    dp[i][j] = min(dp[i][j], q.front().second + prefix);
-                
-                while (!q.empty() && q.back().second >= dp[i][j + 1] - prefix) q.pop_back();
-                q.push_back({i, dp[i][j + 1] == LLONG_MAX ? LLONG_MAX : dp[i][j + 1] - prefix});
-            }
-        }
-
-        return dp[0][0];
+        
+        int n=robot.size(), m=factory.size();
+        memset(dp, -1, sizeof(dp));
+        return f(n-1, m-1, 0, robot, factory);
     }
 };
