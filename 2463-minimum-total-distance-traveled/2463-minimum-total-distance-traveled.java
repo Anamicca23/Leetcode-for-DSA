7246987e97998a2b26496c11301d4f36b9056b2a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-31 20:11:37 +0530

    Time: 16 ms (100%), Space: 41.7 MB (95.38%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,20 +1,23 @@
 import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    public long minimumTotalDistance(int[] robot, int[][] factory) {
-        int n = robot.length, m = factory.length;
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        int n = robot.size(), m = factory.length;
+        int[] robotArray = robot.stream().mapToInt(Integer::intValue).toArray();
+        
         long[] dp = new long[n + 1];
         Arrays.fill(dp, (long) 1e13);
         dp[0] = 0;
-        Arrays.sort(robot);
+        Arrays.sort(robotArray);
         Arrays.sort(factory, (a, b) -> Integer.compare(a[0], b[0]));
-        
+
         for (int i = 0; i < m; i++) {
             int position = factory[i][0];
             int limit = factory[i][1];
             for (int l = 0; l < limit; l++) {
                 for (int j = n - 1; j >= 0; j--) {
-                    dp[j + 1] = Math.min(dp[j + 1], Math.abs(robot[j] - position) + dp[j]);
+                    dp[j + 1] = Math.min(dp[j + 1], Math.abs(robotArray[j] - position) + dp[j]);
                 }
             }
         }
