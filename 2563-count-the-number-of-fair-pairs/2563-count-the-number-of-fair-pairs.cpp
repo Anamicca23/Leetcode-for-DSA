0f84ed112583c33dcb35acdbbd0ccf8a18519331
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-13 23:19:51 +0530

    Time: 59 ms (71.43%), Space: 60.3 MB (61.66%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    long long countLess(vector<int>& nums, int val) {
-    long long res = 0;
-    for (int i = 0, j = nums.size() - 1; i < j; ++i) {
-        while(i < j && nums[i] + nums[j] > val)
-            --j;
-        res += j - i;
-    }        
-    return res;
-}
 long long countFairPairs(vector<int>& nums, int lower, int upper) {
-    sort(begin(nums), end(nums));
-    return countLess(nums, upper) - countLess(nums, lower - 1);
-}
+    sort(nums.begin(),nums.end());
+    int n = nums.size();
+    long long ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        int low=i+1,high=n-1,li=0,ri=0; 
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2,sum = nums[i]+nums[mid];
+            if(lower<=sum && sum<=upper)
+            {
+                low = mid+1;
+                ri = mid;
+            }
+            else if(sum>upper) high = mid-1;
+            else if(sum<lower) low = mid+1;
+        }
+        low=i+1,high=n-1;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2,sum = nums[i]+nums[mid];
+            if(lower<=sum && sum<=upper)
+            {
+                high = mid-1;
+                li = mid;
+            }
+            else if(sum<lower) low = mid+1;
+            else if(sum>upper) high = mid-1;
+        }
+        ans += (ri!=0 && li!=0)?(ri-li)+1:0;
+    }
+    return ans;
+    }
 };
