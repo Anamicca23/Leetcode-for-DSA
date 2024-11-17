dc6c6351bf09f91df4cca192ab3b000c413a5a6e
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-17 23:22:37 +0530

    Time: 218 ms (41.89%), Space: 31.7 MB (55.9%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        res = float("inf")
+
+        cur_sum = 0
+        q = deque()  # (prefix_sum, end_idx)
+
+        for r in range(len(nums)):
+            cur_sum += nums[r]
+            if cur_sum >= k:
+                res = min(res, r + 1)
+
+            # Find the minimum valid window ending at r
+            while q and cur_sum - q[0][0] >= k:
+                prefix, end_idx = q.popleft()
+                res = min(res, r - end_idx)
+
+            # Validate the monotonic deque
+            while q and q[-1][0] > cur_sum:
+                q.pop()
+            q.append((cur_sum, r))
+
+        return -1 if res == float("inf") else res
