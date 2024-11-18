72ca601a0e62ac54cb64741786c560468e41a234
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-18 22:17:25 +0530

    Update 0125-valid-palindrome - 2nd approach using recursion.py

--- 0125-valid-palindrome/0125-valid-palindrome - 2nd approach using recursion.py
+++ 0125-valid-palindrome/0125-valid-palindrome - 2nd approach using recursion.py
@@ -1,4 +1,16 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = "".join(filter(str.isalnum, s.lower()))
-        return s == s[::-1]
+class Solution {
+public:
+    bool helper(int i, int n, vector<char>& chars) {
+        if (i >= n / 2) return true;
+        if (chars[i] != chars[n - i - 1]) return false;
+        return helper(++i, n, chars);
+    }
+
+    bool isPalindrome(string s) {
+        vector<char> chars;
+        for (char c : s) {
+            if (isalnum(c)) chars.push_back(tolower(c));
+        }
+        return helper(0, chars.size(), chars);
+    }
+};
