2fc9b32a32737882129bc2674b3da1fdd135acf9
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-18 22:14:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (41.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newStr = "";
-        
-        for (char c : s) {
-            if (isalnum(c)) {
-                newStr += tolower(c);
+         int start=0, end=s.length()-1;
+        while(start<end) {
+            if (!isalnum(s[start])) start++;
+            else if (!isalnum(s[end])) end--;
+            else {
+                if (tolower(s[start++])!=tolower(s[end--])) return false;
             }
         }
-        
-        string reversed = newStr;
-        reverse(reversed.begin(), reversed.end());
-        
-        return newStr == reversed;
+        return true;    
+    }
+
+    bool isAlphaNumeric(char ch) {
+        auto asciiCode = ch;
+        if((asciiCode >= 'a' && asciiCode <= 'z') || (asciiCode >= '0' && asciiCode <= '9')) return true;
+        return false;
     }
 };
