028f7d67284a67777e32ccdb6a6d42181cef05b0
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-19 21:51:16 +0530

    Time: 3 ms (98.8%), Space: 68 MB (99.26%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,29 +1,27 @@
 class Solution {
+private:
+    long long sum = 0;
+    int dupli = 0, cnt[100001] = {};
+    void pushElement(int &num) {
+        sum += num;
+        if (++cnt[num] == 2) ++dupli;
+    }
+    void popElement(int &num) {
+        sum -= num;
+        if (--cnt[num] == 1) --dupli;
+    }
+        
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long res = 0;
-        unordered_map<int, int> count;
-        long long curSum = 0;
-        
-        int l = 0;
-        for (int r = 0; r < nums.size(); r++) {
-            curSum += nums[r];
-            count[nums[r]]++;
-            
-            if (r - l + 1 > k) {
-                count[nums[l]]--;
-                if (count[nums[l]] == 0) {
-                    count.erase(nums[l]);
-                }
-                curSum -= nums[l];
-                l++;
-            }
-            
-            if (count.size() == k && (r - l + 1) == k) {
-                res = max(res, curSum);
-            }
+        for (int i = 0; i < k; ++i) pushElement(nums[i]);
+        long long res = dupli ? 0 : sum;
+
+        for (int i = 0; i < nums.size() - k; ++i) {
+            popElement(nums[i]);
+            pushElement(nums[i + k]);
+            if (!dupli) res = max(res, sum);
         }
-        
+
         return res;
     }
 };
