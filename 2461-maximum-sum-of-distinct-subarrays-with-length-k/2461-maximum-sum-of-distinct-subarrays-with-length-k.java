ad5ef0b8dec2e73daafeae92460075df61c28d67
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-19 21:51:42 +0530

    Time: 4 ms (99.91%), Space: 55.6 MB (99.46%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,28 +1,38 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long res = 0;
-        Map<Integer, Integer> count = new HashMap<>();
-        long curSum = 0;
-        
-        int l = 0;
-        for (int r = 0; r < nums.length; r++) {
-            curSum += nums[r];
-            count.put(nums[r], count.getOrDefault(nums[r], 0) + 1);
-            
-            if (r - l + 1 > k) {
-                count.put(nums[l], count.get(nums[l]) - 1);
-                if (count.get(nums[l]) == 0) {
-                    count.remove(nums[l]);
-                }
-                curSum -= nums[l];
-                l++;
-            }
-            
-            if (count.size() == r - l + 1 && r - l + 1 == k) {
-                res = Math.max(res, curSum);
-            }
+        int maxNum = 0;
+        for(int num:nums){
+            maxNum = Math.max(maxNum,num);
         }
-        
-        return res;
+        int[] counts = new int[maxNum+1];
+        int dupCount=0;
+        long totalSum=0;
+        long curSum =0;
+        for(int i =0;i<k;i++){
+            if(counts[nums[i]]>=1){
+                dupCount++;
+            }
+            counts[nums[i]]++;
+            curSum+= nums[i];
+        }
+        if(dupCount==0){
+            totalSum=curSum;
+        }
+        for(int i =k;i<nums.length;i++){
+            if(counts[nums[i]]>=1){
+                dupCount++;
+            }
+            counts[nums[i]]++;
+            curSum+= nums[i];  
+            if(counts[nums[i-k]]>1){
+                dupCount--;
+            }
+            counts[nums[i-k]]--;
+            curSum-=nums[i-k];
+            if(dupCount==0){
+                totalSum = Math.max(totalSum,curSum);
+            } 
+        }
+        return totalSum;
     }
 }
