2dbb0da91c708e4f856e9da3b030f5bf36f5abee
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-26 19:43:57 +0530

    Time: 2 ms (76.64%), Space: 93.7 MB (66.24%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
         vector<int>freq(n);
-        for(auto&x:edges){
-            int v = x[0], e = x[1];
-            freq[e]++;
-        }
+        for(auto&x:edges){int e = x[1];freq[e]++;}
         int champ =-1, cnt=0;
         for(int i=0; i<freq.size();i++){
             if(freq[i]==0){
