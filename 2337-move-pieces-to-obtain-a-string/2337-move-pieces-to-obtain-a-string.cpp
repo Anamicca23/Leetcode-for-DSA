3dad23865d3b1083fdd58ec20840545719a6189f
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-12-05 22:16:33 +0530

    Time: 9 ms (64.37%), Space: 18.4 MB (99.55%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool canChange(string& s, string& t) {
+        const int n=s.size();
+        for(int i=0, j=0; i<n || j<n; i++, j++){
+            while(i<n && s[i]=='_') i++;
+            while(j<n && t[j]=='_') j++;
+        //    cout<<i<<", "<<j<<endl;
+            char c=s[i];
+            if (c !=t[j]) return 0;
+            if (c =='L' && i<j) return 0;
+            if (c =='R' && i>j) return 0;
+        }
+        return 1;
+    }
+};
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
