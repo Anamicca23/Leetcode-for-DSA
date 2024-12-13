350d500f11e9f194c7a3a5af75970694cc44523d
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-12-14 01:31:35 +0530

    Time: 115 ms (71.92%), Space: 105.8 MB (51.57%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -3,25 +3,26 @@ public:
     #define P pair<int,int>
     long long findScore(vector<int>& nums) {
         int n = nums.size();
-        vector<bool>visited(n,false);
         priority_queue<P,vector<P>, greater<P>>minheap;
+        vector<bool>visited(n,false);
         for(int i =0; i<n; i++){
             minheap.push({nums[i],i});
         }
-        long long score=0;
+        long long score =0;
         while(!minheap.empty()){
             P temp = minheap.top();
-            int element = temp.first;
-            int idx = temp.second;
+            int elem = temp.first, idx = temp.second;
             minheap.pop();
 
             if(!visited[idx]){
                 visited[idx]=true;
-                score+=element;
-                if(idx-1>=0 and !visited[idx-1])
+                score+=elem;
+                if(idx-1>=0 and !visited[idx-1]){
                     visited[idx-1]=true;
-                if(idx+1<n and !visited[idx+1])
+                }
+                if(idx+1<n and !visited[idx+1]){
                     visited[idx+1]=true;
+                }
             }
         }
         return score;
