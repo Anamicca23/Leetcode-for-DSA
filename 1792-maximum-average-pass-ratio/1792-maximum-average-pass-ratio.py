c2ca316f1b0f57e22bd73fd96790de4b65807926
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-12-15 23:55:33 +0530

    Time: 907 ms (96%), Space: 54.3 MB (37.04%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
@@ -1,23 +1,11 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        import heapq
+        classes = [(num/denom - (num+1)/(denom+1), num, denom) for num, denom in classes]
+        heapify(classes)
+        if classes[0][0]==0:
+            return 1
+        for i in range(extraStudents):
+            _, num, denom = heappop(classes)
+            heappush(classes, ((num+1)/(denom+1) - (num+2)/(denom+2), num+1, denom+1))
 
-        def gain(pass_, total):
-            return (pass_ + 1) / (total + 1) - pass_ / total
-
-        max_heap = []
-        sum_ = 0.0
-
-        for pass_, total in classes:
-            sum_ += pass_ / total
-            heapq.heappush(max_heap, (-gain(pass_, total), pass_, total))
-
-        for _ in range(extraStudents):
-            current_gain, pass_, total = heapq.heappop(max_heap)
-            sum_ -= pass_ / total
-            pass_ += 1
-            total += 1
-            sum_ += pass_ / total
-            heapq.heappush(max_heap, (-gain(pass_, total), pass_, total))
-
-        return sum_ / len(classes)
+        return sum([c[1]/c[2] for c in classes])/len(classes)
