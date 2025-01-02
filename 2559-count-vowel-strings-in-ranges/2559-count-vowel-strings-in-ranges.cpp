ca4407d062590036671ce0c9638ea3dc04c9a773
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-01-02 23:12:39 +0530

    Time: 0 ms (100%), Space: 69.2 MB (74.2%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     bool isvowel(char c){
-        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
-           return true;
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')return true;
         return false;   
     }
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        int q = queries.size();
-        int n = words.size();
-
-        vector<int>res(q);
-        vector<int>csum(n);
+        int q = queries.size(),n = words.size();
+        vector<int>res(q),csum(n);
         int sum=0;
         for(int i=0; i<n; i++){
-            if(isvowel(words[i][0]) and isvowel(words[i].back())){
-                sum++;
-            }
+            if(isvowel(words[i][0]) and isvowel(words[i].back()))sum++;
             csum[i]=sum;
         }
         for(int i=0; i<q;i++){
