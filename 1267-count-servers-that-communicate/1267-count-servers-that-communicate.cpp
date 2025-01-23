7f80caaffdf5060b916a375edf258fe99c14743a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-01-23 05:49:26 +0530

    Time: 0 ms (100%), Space: 26.8 MB (39.9%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int countServers(vector<vector<int>>& grid) 
-    {
-        vector<int> rows(grid.size(),0),columns(grid[0].size(),0);	
-        for(int i=0;i<grid.size();i++)			
-            for(int j=0;j<grid[i].size();j++)
-                if(grid[i][j])
-                    rows[i]++,columns[j]++;
-        int result=0;
-        for(int i=0;i<grid.size();i++)			
-            for(int j=0;j<grid[i].size();j++)
-                if(grid[i][j]&&(columns[j]>1||rows[i]>1))	
+    int countServers(vector<vector<int>>& grid) {
+        vector<int> rows(grid.size(), 0), columns(grid[0].size(), 0);
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[i].size(); j++)
+                if (grid[i][j])
+                    rows[i]++, columns[j]++;
+        int result = 0;
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[i].size(); j++)
+                if (grid[i][j] && (columns[j] > 1 || rows[i] > 1))
                     result++;
         return result;
     }
