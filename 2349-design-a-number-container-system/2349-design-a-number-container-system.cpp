e99863ba64736e9bf4718ac3d70cb6dd82fa8075
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-02-08 23:39:13 +0530

    Time: 92 ms (90%), Space: 185.8 MB (89.69%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -1,22 +1,30 @@
 class NumberContainers {
 public:
-    map<int, int> mp;
-    map<int, set<int>> mpp;
-    
-    NumberContainers() {}
-
-    void change(int index, int number) {
-        if(mp.find(index) != mp.end() && mp[index] != number) {
-            mpp[mp[index]].erase(index);  
-            if(mpp[mp[index]].empty()) {
-                mpp.erase(mp[index]);
-            }
-        }
-        mp[index] = number;
-        mpp[number].insert(index);
+  void change(int index, int number) {
+    entries[index] = number;
+    number_to_indexes[number].push(index);
+  }
+  
+  int find(int number) {
+    if (auto find_it = number_to_indexes.find(number);
+        find_it == end(number_to_indexes)) {
+      return -1;
+    } else {
+      auto& q = find_it->second;
+      while (!empty(q) && entries[q.top()] != number) q.pop();
+      return empty(q) ? -1 : q.top();
     }
+  }
 
-    int find(int number) {
-        return mpp[number].empty() ? -1 : *mpp[number].begin();
-    }
+private:
+  unordered_map<int, priority_queue<int, vector<int>, greater<int>>>
+      number_to_indexes;
+  unordered_map<int, int> entries;
 };
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
