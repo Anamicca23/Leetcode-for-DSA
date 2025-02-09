98c0a42bde642870b15d886e9b9ed57056d2f09a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-02-09 19:47:21 +0530

    Time: 71 ms (47.87%), Space: 87.8 MB (52.89%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        long long n=nums.size();
+        long long totalPairs=(n*(n-1))/2;
+        unordered_map<int ,int>mp;
+        long long notBadPairs=0;
+        for(int i=0;i<n;i++)
+        {
+         // if any such element exist before i , which has nums[j]-j =nums[i]-i
+            if(mp[nums[i]-i]!=0)
+            {
+                notBadPairs+=mp[nums[i]-i];
+            }
+            mp[nums[i]-i]+=1;
+        }
+
+        return totalPairs-notBadPairs;
+    }
+};
