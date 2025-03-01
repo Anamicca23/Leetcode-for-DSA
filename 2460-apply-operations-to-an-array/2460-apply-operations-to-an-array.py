ed9b52bf06e1ea10034ba9234d321406107b8557
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-03-01 13:59:33 +0530

    Time: 0 ms (100%), Space: 18 MB (46.47%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -1,25 +1,14 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        
-        # Apply operations
-        for i in range(n - 1):
-            if nums[i] == nums[i + 1]:
-                nums[i] *= 2
-                nums[i + 1] = 0
-        
-        # Shift zeros to the end (in-place)
-        non_zero_idx = 0
-        
-        # Move all non-zero elements to the front
-        for i in range(n):
-            if nums[i] != 0:
-                nums[non_zero_idx] = nums[i]
-                non_zero_idx += 1
-        
-        # Fill the remaining positions with zeros
-        for i in range(non_zero_idx, n):
-            nums[i] = 0
-        
+        i=0
+        while i<len(nums)-1:
+            if nums[i]==nums[i+1]:
+                nums[i]*=2
+                nums[i+1]=0
+            i+=1
+        j=0
+        for k in range(len(nums)):
+            if nums[k]!=0:
+                nums[k],nums[j]=nums[j],nums[k]
+                j+=1
         return nums
-        
