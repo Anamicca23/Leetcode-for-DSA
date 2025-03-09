cc7bd93595886bc2a7faa4ceb7e23d7f7d17cec4
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-03-09 10:07:35 +0530

    Time: 0 ms (100%), Space: 51.4 MB (71.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,53 @@
-import java.util.*;
+import java.util.AbstractList;
 
 class Solution {
-    private List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] arr) {
+		return new AbstractList<>() {
+			List<List<Integer>> ans;
 
-    private void twosum(int[] nums, int targ, int i, int j) {
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-            if (sum < targ) {
-                i++;
-            } else if (sum > targ) {
-                j--;
-            } else {
-                res.add(Arrays.asList(-targ, nums[i], nums[j]));
-                while (i < j && nums[i] == nums[i + 1]) i++;
-                while (i < j && nums[j] == nums[j - 1]) j--;
-                i++;
-                j--;
-            }
-        }
-    }
+			@Override
+			public int size() {
+				if (ans == null)
+					ans = createList(arr);
+				return ans.size();
+			}
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
-        if (n < 3) return res;
+			@Override
+			public List<Integer> get(int index) {
+				if (ans == null)
+					ans = createList(arr);
+				return ans.get(index);
+			}
+		};
+	}
 
-        Arrays.sort(nums);
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;  // skip duplicates
-            int targ = -nums[i];
-            twosum(nums, targ, i + 1, n - 1);
-        }
-        return res;
-    }
+	private List<List<Integer>> createList(int[] arr) {
+		List<List<Integer>> ans = new ArrayList<>();
+		int len = arr.length;
+		Arrays.sort(arr);
+		for (int i = 0; i < len - 2 && arr[i] <= 0; ++i) {
+			if (i > 0 && arr[i] == arr[i - 1])
+				continue;
+			twoSum(ans, arr, i + 1, len - 1, -arr[i]);
+		}
+		return ans;
+	}
+
+	private void twoSum(List<List<Integer>> ans, int[] arr, int left, int right, int target) {
+		while (left < right) {
+			if (arr[left] + arr[right] > target) {
+				--right;
+				continue;
+			}
+			if (arr[left] + arr[right] < target) {
+				++left;
+				continue;
+			}
+
+			ans.add(Arrays.asList(-target, arr[left++], arr[right--]));
+
+			while (left <= right && arr[left] == arr[left - 1])
+				++left;
+		}
+	}
 }
