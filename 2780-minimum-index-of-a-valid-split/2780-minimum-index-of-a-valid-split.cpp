4b9cf1690427e997565308b5ebd98dd3e90c8843
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-03-27 13:50:52 +0530

    Time: 0 ms (100%), Space: 89.8 MB (93.85%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -1,39 +1,21 @@
 class Solution {
-    int mooresVotingAlgo(vector<int>& nums){
-        int majority_element = nums[0];
-        int freq = 1;
-        for(int i=1;i<nums.size();++i){
-            if(nums[i]!=majority_element)   freq--;
-            else                            freq++;
-            
-            if(freq==0){
-                majority_element = nums[i];
-                freq = 1;
-            }
+    int mooresVotingAlgo(vector<int>& nums) {
+        int maj = nums[0], freq = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            freq += (nums[i] == maj) ? 1 : -1;
+            if (freq == 0) maj = nums[i], freq = 1;
         }
-        return majority_element;
+        return maj;
     }
 public:
     int minimumIndex(vector<int>& nums) {
-        int n=nums.size();
-        //Step-1: Find Majority element
-        int majority_element = mooresVotingAlgo(nums);
+        int n = nums.size(), maj = mooresVotingAlgo(nums), cnt = 0;
+        for (int x : nums) cnt += (x == maj);
 
-        //Step-2: Count frequency of majority_element
-        int max_freq = 0;
-        for(int i=0;i<n;++i)
-            if(nums[i]==majority_element)
-                max_freq++;
-
-        //Step-3: Now find minimum valid split
-        int prefix_count = 0;
-        for(int i=0;i<n-1;++i){
-            if(nums[i]==majority_element){
-                prefix_count++;
-                max_freq--;
-            }
-            if((prefix_count > (i+1)/2) and (max_freq > (n-i-1)/2))
-                return i;
+        int pre = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums[i] == maj) pre++, cnt--;
+            if (pre > (i + 1) / 2 && cnt > (n - i - 1) / 2) return i;
         }
         return -1;
     }
