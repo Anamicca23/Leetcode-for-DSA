c7a4b9aebc08dd8092e4b8fc445f23b2e8cde5e7
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-04-29 16:15:42 +0530

    Time: 13 ms (15.88%), Space: 121.8 MB (20.29%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,46 +1,21 @@
-typedef long long int lli;
-
 class Solution {
 public:
-	long long countSubarrays(vector<int>& nums, int k) {
-		lli ret = 0;
-		lli val = *max_element(nums.begin(), nums.end());
-		int n = nums.size();
-		vector<lli> pre(n);
-		for (int i = 0; i < n; i++) {
-			pre[i] = val == nums[i];
-			if(i)pre[i] += pre[i - 1];
-		}
-    // for(int i : pre){
-    //     cout << i << " ";
-    // }
-    //     cout << endl;
-		return Calc(pre, n) - Calc(pre, k - 1);
-	}
-	lli Calc( vector<lli>& pre, int k) {
-		lli ret = 0;
-		int n = pre.size();
-		for (int i = 0; i < n; i++) {
-			lli add = 0;
-			lli low = 0, high = i;
-			while (low <= high) {
-				lli mid = low + (high - low) / 2;
-                lli val = pre[i] - (mid ? pre[mid - 1] : 0);
-                // cout << i << " " << mid << " --" << val << endl;
-				if ( val > k) {
-					low = mid + 1;
-				
-				
-				} else {
-					high = mid - 1;
-					add = i - mid  + 1;
-                    // cout << "Yes " << add << " " << i << " " << mid <<  endl;
-				}
-			}
-            // cout << i << " " << k << " " << add << endl;
-			ret += add;
-		}
-        // cout << ret << endl;
-		return ret;
-	}
+    long long countSubarrays(vector<int>& nums, int k) {
+        long long int max_num = *max_element(nums.begin(),nums.end()),count = 0;
+        long long int left = 0,right = 0,ans = 0;
+        while(right<nums.size()){
+            if(nums[right] == max_num)count++;
+            
+            while(count>=k){
+                if(nums[left]==max_num)count--;
+                left++;
+                ans += nums.size()-right;
+            }    
+            right++;
+        }
+
+
+        return ans;
+        
+    }
 };
