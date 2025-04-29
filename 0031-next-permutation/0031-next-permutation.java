fdc57fa65f1b4bbd2a87bac53fe36259e32a6307
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-04-29 17:16:07 +0530

    Time: 0 ms (100%), Space: 43.2 MB (56.31%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        // Step 1: Find the first decreasing element from the end
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        // Step 2: Find the next larger element and swap
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+}
