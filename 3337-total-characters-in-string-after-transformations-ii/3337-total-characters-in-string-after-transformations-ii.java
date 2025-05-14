da4cdb14ef803237c2d85a13cf38764728b1c1bf
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-05-14 21:31:43 +0530

    Time: 67 ms (94.59%), Space: 45.7 MB (24.32%) - LeetHub

--- 3337-total-characters-in-string-after-transformations-ii/3337-total-characters-in-string-after-transformations-ii.java
+++ 3337-total-characters-in-string-after-transformations-ii/3337-total-characters-in-string-after-transformations-ii.java
@@ -1,71 +1,77 @@
-import java.util.*;
+public class Solution {
+    public static final int MOD = 1000000007;
+    public static final long M2 = (long) MOD * MOD;
+    public static final long BIG = 8L * M2;
 
-class Solution {
-    static final int MOD = 1_000_000_007;
-    
-    // Matrix multiplication
-    private long[][] multiply(long[][] a, long[][] b) {
-        long[][] res = new long[26][26];
-        for (int i = 0; i < 26; ++i) {
-            for (int k = 0; k < 26; ++k) {
-                if (a[i][k] == 0) continue;
-                for (int j = 0; j < 26; ++j) {
-                    res[i][j] = (res[i][j] + a[i][k] * b[k][j]) % MOD;
+    public int lengthAfterTransformations(String s, int t, List<Integer> nums) {
+        int[][] m = new int[26][26];
+        for (int i = 0; i < 26; i++) {
+            for (int j = 1; j <= nums.get(i); j++) {
+                m[(i + j) % 26][i]++;
+            }
+        }
+        int[] v = new int[26];
+        for (char c : s.toCharArray()) {
+            v[c - 'a']++;
+        }
+        v = pow(m, v, t);
+        long ans = 0;
+        for (int x : v) {
+            ans += x;
+        }
+        return (int) (ans % MOD);
+    }
+
+    private int[] pow(int[][] a, int[] v, long e) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] >= MOD) {
+                v[i] %= MOD;
+            }
+        }
+        int[][] mul = a;
+        for (; e > 0; e >>>= 1) {
+            if ((e & 1) == 1) {
+                v = mul(mul, v);
+            }
+            mul = p2(mul);
+        }
+        return v;
+    }
+
+    private int[] mul(int[][] a, int[] v) {
+        int m = a.length;
+        int n = v.length;
+        int[] w = new int[m];
+        for (int i = 0; i < m; i++) {
+            long sum = 0;
+            for (int k = 0; k < n; k++) {
+                sum += (long) a[i][k] * v[k];
+                if (sum >= BIG) {
+                    sum -= BIG;
                 }
             }
+            w[i] = (int) (sum % MOD);
         }
-        return res;
+        return w;
     }
-    
-    // Matrix exponentiation
-    private long[][] matrixPow(long[][] mat, int power) {
-        long[][] result = new long[26][26];
-        for (int i = 0; i < 26; ++i) {
-            result[i][i] = 1;
-        }
-        while (power > 0) {
-            if (power % 2 == 1) {
-                result = multiply(result, mat);
+
+    private int[][] p2(int[][] a) {
+        int n = a.length;
+        int[][] c = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            long[] sum = new long[n];
+            for (int k = 0; k < n; k++) {
+                for (int j = 0; j < n; j++) {
+                    sum[j] += (long) a[i][k] * a[k][j];
+                    if (sum[j] >= BIG) {
+                        sum[j] -= BIG;
+                    }
+                }
             }
-            mat = multiply(mat, mat);
-            power /= 2;
-        }
-        return result;
-    }
-    
-    public int lengthAfterTransformations(String s, int t, List<Integer> nums) {
-        // Initialize the transition matrix
-        long[][] transition = new long[26][26];
-        for (int c = 0; c < 26; ++c) {
-            int num = nums.get(c);
-            for (int j = 1; j <= num; ++j) {
-                int nextChar = (c + j) % 26;
-                transition[c][nextChar]++;
+            for (int j = 0; j < n; j++) {
+                c[i][j] = (int) (sum[j] % MOD);
             }
         }
-        
-        // Raise the transition matrix to the t-th power
-        long[][] matPow = matrixPow(transition, t);
-        
-        // Initialize the initial count vector
-        long[] cnt = new long[26];
-        for (char c : s.toCharArray()) {
-            cnt[c - 'a']++;
-        }
-        
-        // Multiply the count vector by the matrix^t
-        long[] newCnt = new long[26];
-        for (int i = 0; i < 26; ++i) {
-            for (int j = 0; j < 26; ++j) {
-                newCnt[j] = (newCnt[j] + cnt[i] * matPow[i][j]) % MOD;
-            }
-        }
-        
-        // Sum all the counts
-        long total = 0;
-        for (long x : newCnt) {
-            total = (total + x) % MOD;
-        }
-        return (int)total;
+        return c;
     }
 }
