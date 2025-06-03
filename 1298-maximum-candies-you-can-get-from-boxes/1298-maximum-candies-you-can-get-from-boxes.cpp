15acb86c39c13b498d65fa7818ea5e32663470ec
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-06-04 00:53:26 +0530

    Time: 6 ms (46.15%), Space: 45.1 MB (25.48%) - LeetHub

--- 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.cpp
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int dfs(int boxes, vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, 
-    unordered_set<int>&visited,unordered_set<int>&foundboxes){
-        if(visited.count(boxes))return 0;
-        if(status[boxes]==0){foundboxes.insert(boxes);return 0;}
+    int dfs(int boxes, vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes , 
+    unordered_set<int>&visited, unordered_set<int>&foundboxes){
+        if(visited.count(boxes))return 0; // if box is visited we can't collect any condies.
+        if(status[boxes]==0){foundboxes.insert(boxes); return 0;} // if box is closed just add it in foundboxes and we can't get any candies form closed ones .
+        
         visited.insert(boxes);
         int collectedcandies = candies[boxes];
 
-        for(int &insidebox:containedBoxes[boxes]){
-            collectedcandies += dfs(insidebox,status, candies, keys,containedBoxes, visited, foundboxes);
+        for(int &insideboxes: containedBoxes[boxes]){
+            collectedcandies += dfs(insideboxes, status, candies, keys, containedBoxes, visited, foundboxes); // collect all candies which is on opened boxes.
         }
-        for(int &keyboxes: keys[boxes]){
-            status[keyboxes]=1;
-            if(foundboxes.count(keyboxes)){
-                collectedcandies += dfs(keyboxes, status, candies, keys, containedBoxes, visited, foundboxes);
+        for(int &keyfoundbox: keys[boxes]){
+            status[keyfoundbox]=1;  // mark status opened whose key has been founded.
+            if(foundboxes.count(keyfoundbox)){ //if that closed box if in foundedbox then only add candies . 
+              collectedcandies += dfs(keyfoundbox, status, candies, keys, containedBoxes, visited, foundboxes);
             }
         }
         return collectedcandies;
     }
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        int collectedcandies =0;
-        unordered_set<int>visited;
-        unordered_set<int>foundboxes;
+        int collectedcandies = 0;
+        unordered_set<int> visited;
+        unordered_set<int> foundboxes;
         for(int &box: initialBoxes){
-            collectedcandies += dfs(box, status, candies,keys, containedBoxes,visited, foundboxes);
+            collectedcandies += dfs(box, status, candies, keys, containedBoxes, visited, foundboxes);
         }
         return collectedcandies;
     }
