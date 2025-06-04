bf7fcdf405028d1a257f39a33a7c6ca306c67a91
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-06-04 23:10:38 +0530

    Time: 74 ms (43.66%), Space: 169.3 MB (24.63%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -5,7 +5,7 @@ public:
         if(numFriends == 1)return word;
         string res = "";
         for(int i =0; i<n; i++){
-           res = max(res, word.substr(i, n - numFriends +1));
+            res = max(res, word.substr(i,n-numFriends+1));
         }
         return res;
     }
