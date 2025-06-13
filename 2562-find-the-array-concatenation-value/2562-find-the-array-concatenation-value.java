a8dda83b4b2402d921b7fd5529875870a48c422a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-06-13 15:43:19 +0530

    Time: 0 ms (100%), Space: 42.8 MB (97.63%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
@@ -1,20 +1,29 @@
 class Solution {
     public long findTheArrayConcVal(int[] nums) {
-        long m=0;
-        if(nums.length==1)
-        {
-            return (long)(nums[0]);
+        long res = 0L;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int a = nums[left++];
+            int b = nums[right--];
+            res += a * pow10(b) + b;
         }
-        for(int i=0,j=nums.length-1;i<j;i++,j--)
-        {
-            String p=String.valueOf(nums[i])+String.valueOf(nums[j]);
-            long q=Long.parseLong(p);
-            m+=q;
+
+        if (left == right) {
+            res += nums[left];
         }
-        if(nums.length%2==1)
-        {
-            return m+(long)(nums[nums.length/2]);
+
+        return res;
+    }
+
+    // Helper to calculate 10^digits(b)
+    private long pow10(int num) {
+        long pow = 1;
+        while (num > 0) {
+            pow *= 10;
+            num /= 10;
         }
-        return m;
+        return pow;
     }
 }
