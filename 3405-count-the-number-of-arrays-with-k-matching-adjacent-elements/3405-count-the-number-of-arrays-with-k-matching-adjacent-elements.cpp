317f33138bd9e4b2f7659b1deacb23918beac8b6
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-06-18 03:10:47 +0530

    Time: 1 ms (91.58%), Space: 9.1 MB (82.11%) - LeetHub

--- 3405-count-the-number-of-arrays-with-k-matching-adjacent-elements/3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
+++ 3405-count-the-number-of-arrays-with-k-matching-adjacent-elements/3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -1,29 +1,47 @@
+const int mod = 1e9 + 7;
+int fact[100000] = {0};
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-
-    long long modPow(long long a, long long b) {
-        long long res = 1;
-        while (b > 0) {
-            if (b % 2 == 1)
-                res = res * a % MOD;
-            a = a * a % MOD;
-            b /= 2;
+    static void comp_fact(){
+        if (fact[0] == 1)
+            return;
+        long long p = 1;
+        fact[0] = 1;
+        for (int i = 1; i < 100000; i++){
+            p *= i;
+            if (p >= mod) p %= mod;
+            fact[i] = p;
         }
-        return res;
     }
-
-    long long modInv(long long x) {
-        return modPow(x, MOD - 2);
+    static inline long long modPow(long long x, unsigned exp, int Mod = mod){
+        if (exp == 0) return 1;
+        int bits = 31 - countl_zero(exp);
+        bitset<32> B(exp);
+        long long ans = x;
+        for (int b = bits - 1; b >= 0; b--){
+            ans = (ans * ans % Mod) * (B[b] ? x : 1) % Mod;
+        }
+        return ans;
     }
-
+    static inline int Inv(int a, int b = mod){
+        int x0 = 1, x1 = 0;
+        int r0 = a, r1 = b;
+        while (r1 != 0){
+            int q = r0 / r1, rr = r1, xx = x1;
+            r1= r0 - q * r1;
+            x1 = x0 - q * x1;
+            r0 = rr;
+            x0 = xx;
+        }
+        if (x0 < 0)
+            x0 += b;
+        return x0;
+    }
+    static long long comb(int n, int k){
+        return 1LL * fact[n] * Inv(fact[k]) % mod * Inv (fact[n - k]) % mod;
+    }
     int countGoodArrays(int n, int m, int k) {
-        vector<long long> fact(n, 1);
-        for (int i = 1; i < n; ++i)
-            fact[i] = fact[i - 1] * i % MOD;
-
-        long long comb = fact[n - 1] * modInv(fact[k]) % MOD * modInv(fact[n - 1 - k]) % MOD;
-        long long res = comb * m % MOD * modPow(m - 1, n - 1 - k) % MOD;
-        return res;
+        comp_fact();
+        return m * modPow(m - 1, n - k - 1) % mod * comb(n - 1, k) % mod;
     }
 };
