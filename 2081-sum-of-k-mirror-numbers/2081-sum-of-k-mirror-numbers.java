5dcb56e8c944d4a5de4bd9a69430edcad0fc3b2b
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-06-23 21:53:22 +0530

    Time: 389 ms (39.29%), Space: 44.8 MB (32.14%) - LeetHub

---
+++ 2081-sum-of-k-mirror-numbers/2081-sum-of-k-mirror-numbers.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    long createPalindrome(long num, boolean odd) {
+        long x = num;
+        if (odd) x /= 10;
+        while (x > 0) {
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+        return num;
+    }
+
+    boolean isPalindrome(long num, int base) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append((char) (num % base + '0'));
+            num /= base;
+        }
+        String s = sb.toString();
+        int i = 0, j = s.length() - 1;
+        while (i < j) if (s.charAt(i++) != s.charAt(j--)) return false;
+        return true;
+    }
+
+    public long kMirror(int k, int n) {
+        long sum = 0;
+        for (long len = 1; n > 0; len *= 10) {
+            for (long i = len; n > 0 && i < len * 10; i++) {
+                long p = createPalindrome(i, true);
+                if (isPalindrome(p, k)) { sum += p; n--; }
+            }
+            for (long i = len; n > 0 && i < len * 10; i++) {
+                long p = createPalindrome(i, false);
+                if (isPalindrome(p, k)) { sum += p; n--; }
+            }
+        }
+        return sum;
+    }
+}
