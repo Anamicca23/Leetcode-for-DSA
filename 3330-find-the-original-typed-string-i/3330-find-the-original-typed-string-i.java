9baabea9a79d101ae7131af8609400e5f6353f96
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-07-01 23:10:28 +0530

    Time: 1 ms (87.19%), Space: 42.3 MB (38.84%) - LeetHub

---
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int n=word.length();
+        int count=n;
+        for(int i=1;i<word.length();i++){
+            if(word.charAt(i)!=word.charAt(i-1)){
+                count--;
+            }
+        }
+        return count;
+    }
+}
