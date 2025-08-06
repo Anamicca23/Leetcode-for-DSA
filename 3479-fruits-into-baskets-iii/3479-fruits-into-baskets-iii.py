1c038476e51690ff91be4148ca717c55ea0b7aae
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-08-06 05:49:02 +0530

    Time: 2744 ms (49.67%), Space: 36.7 MB (88.59%) - LeetHub

--- 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.py
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.py
@@ -1,40 +1,37 @@
 class SegmentTree:
-    def __init__(self, n: int, baskets: list[int]) -> None:
-        self.seg_tree: list[int] = [0] * (4 * 100 + 1)
-        self.build(1, 0, n, baskets)
+    def __init__(self, leaf_nodes):
+        l = len(leaf_nodes)
+        self.n = 2 ** math.ceil(math.log2(l))
+        self.nodes = [0] * (2 * self.n)
+        for i, val in enumerate(leaf_nodes):
+            self.update(i + self.n, val)
     
-    def build(self, parent: int, left: int, right: int, baskets: list[int]) -> None:
-        if left == right:
-            self.seg_tree[parent] = baskets[left]
-            return
-        middle: int = left + ((right - left) >> 1)
-        self.build(parent << 1, left, middle, baskets)
-        self.build(parent << 1 | 1, middle + 1, right, baskets)
-        self.seg_tree[parent] = max(self.seg_tree[parent << 1], self.seg_tree[parent << 1 | 1])
+    def update(self, i, val):
+        self.nodes[i] = val
+        while i > 1:
+            i = i // 2
+            self.nodes[i] = max(self.nodes[i*2], self.nodes[i*2+1])
     
-    def query(self, parent: int, left: int, right: int, target: int) -> int:
-        if self.seg_tree[parent] < target: return -1
-        elif left == right: return left
-        middle: int = left + ((right - left) >> 1)
-        if self.seg_tree[parent << 1] >= target: return self.query(parent << 1, left, middle, target)
-        return self.query(parent << 1 | 1, middle + 1, right, target)
-    
-    def update(self, parent: int, left: int, right: int, pos: int, val: int) -> None:
-        if left == right:
-            self.seg_tree[parent] = val
-            return
-        middle: int = left + ((right - left) >> 1)
-        if pos <= middle: self.update(parent << 1, left, middle, pos, val)
-        else: self.update(parent << 1 | 1, middle + 1, right, pos, val)
-        self.seg_tree[parent] = max(self.seg_tree[parent << 1], self.seg_tree[parent << 1 | 1])
-
+    def find(self, target_val):
+        if self.nodes[1] < target_val:
+            return -1
+        i = 1
+        while i < self.n:
+            if self.nodes[i * 2] >= target_val:
+                i = i * 2
+            else:
+                i = i * 2 + 1
+        return i
+        
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
-        n: int = len(fruits)
-        st: SegmentTree = SegmentTree(n - 1, baskets)
-        output: int = 0
-        for fruit in fruits:
-            pos: int = st.query(1, 0, n - 1, fruit)
-            if pos == -1: output += 1
-            else: st.update(1, 0, n - 1, pos, -1)
-        return output
+        s_t = SegmentTree(baskets)
+        res = 0
+        for val in fruits:
+            node_id = s_t.find(val)
+            if node_id != -1:
+                s_t.update(node_id, -1)
+            else:
+                res += 1
+        return res
+
