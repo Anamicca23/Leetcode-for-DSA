d2a209f62dadec3410e160ad5d9a8b398bbe7a95
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-10-13 23:19:59 +0530

    Time: 10 ms (12.14%), Space: 21.6 MB (6.94%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
         vector<unordered_map<char, int>> freq(words.size());
-
         for (int i = 0; i < words.size(); i++) {
             for (char ch : words[i]) freq[i][ch]++;
         }
-
         vector<string> ans;
         ans.push_back(words[0]);
         for (int i = 1; i < words.size(); i++) {
