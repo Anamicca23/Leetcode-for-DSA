e76f266a8d02f157b6e6175d27301b92400c60e1
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-10-30 21:55:07 +0530

    Time: 3 ms (100%), Space: 56.9 MB (50.7%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public int minNumberOperations(int[] target) {
-        int min = target[0]; 
+        int min = target[0]; // Start with the first element
         for (int i = 1; i < target.length; i++) {
+            // Add only when there's an increase compared to the previous number
             min += Math.max(target[i] - target[i - 1], 0);
         }
         return min;
