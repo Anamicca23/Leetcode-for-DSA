f6f9011954c2de389d9bb6dcec1f532d85355337
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-11-02 22:08:40 +0530

    Time: 643 ms (29.59%), Space: 49.5 MB (12.25%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,43 +1,42 @@
 class Solution:
-    def mark_guarded(self, row, col, grid):
-        # Mark cells in the up direction
-        for i in range(row - 1, -1, -1):
-            if grid[i][col] in (2, 3):
-                break
-            grid[i][col] = 1
-        
-        # Mark cells in the down direction
-        for i in range(row + 1, len(grid)):
-            if grid[i][col] in (2, 3):
-                break
-            grid[i][col] = 1
-        
-        # Mark cells in the left direction
-        for j in range(col - 1, -1, -1):
-            if grid[row][j] in (2, 3):
-                break
-            grid[row][j] = 1
-        
-        # Mark cells in the right direction
-        for j in range(col + 1, len(grid[0])):
-            if grid[row][j] in (2, 3):
-                break
-            grid[row][j] = 1
+    def dfs(self, r, c, dir, vis, mp):
+        n, m = len(vis), len(vis[0])
+        if r < 0 or c < 0 or r >= n or c >= m:
+            return
+        if (r, c) in mp:
+            return
+        vis[r][c] = 1
+
+        if dir == "r":
+            self.dfs(r, c + 1, "r", vis, mp)
+        if dir == "l":
+            self.dfs(r, c - 1, "l", vis, mp)
+        if dir == "u":
+            self.dfs(r - 1, c, "u", vis, mp)
+        if dir == "d":
+            self.dfs(r + 1, c, "d", vis, mp)
 
     def countUnguarded(self, m, n, guards, walls):
-        grid = [[0] * n for _ in range(m)]
+        vis = [[0] * n for _ in range(m)]
+        mp = {}
 
-        # Mark guards as 2 in the grid
-        for i, j in guards:
-            grid[i][j] = 2
-        
-        # Mark walls as 3 in the grid
-        for i, j in walls:
-            grid[i][j] = 3
-        
-        # Mark guarded cells
-        for i, j in guards:
-            self.mark_guarded(i, j, grid)
-        
-        # Count unguarded cells
-        return sum(1 for i in range(m) for j in range(n) if grid[i][j] == 0)
+        for r, c in guards:
+            mp[(r, c)] = 1
+            vis[r][c] = 1
+
+        for r, c in walls:
+            mp[(r, c)] = 1
+            vis[r][c] = 1
+
+        for r, c in guards:
+            self.dfs(r, c + 1, "r", vis, mp)
+            self.dfs(r, c - 1, "l", vis, mp)
+            self.dfs(r + 1, c, "d", vis, mp)
+            self.dfs(r - 1, c, "u", vis, mp)
+
+        cnt = 0
+        for i in range(m):
+            for j in range(n):
+                if vis[i][j] == 0:
+                    cnt += 1
+        return cnt
