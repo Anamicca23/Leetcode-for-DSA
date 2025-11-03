d165a114c5f26716ef6a3590241b9e02900c7b2f
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-11-03 22:21:07 +0530

    Time: 5 ms (42.91%), Space: 100 MB (35.11%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
         int n = colors.size(), sum = 0;
+        
         for (int i = 1; i < n; i++) {
-            int maxi = 0;
+            int maxi = 0; // largest value balloon in substring
             while (i < n and colors[i] == colors[i - 1]) {
                 sum += neededTime[i - 1];
                 maxi = max(maxi, neededTime[i - 1]);
