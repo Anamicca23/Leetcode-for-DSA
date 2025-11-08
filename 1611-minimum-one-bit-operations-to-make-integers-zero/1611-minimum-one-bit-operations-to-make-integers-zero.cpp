563c082229b7608922720df0ceb50bead3e32870
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-11-08 23:56:22 +0530

    Time: 0 ms (100%), Space: 8 MB (55.27%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+};
