6f4a07afa9c4d22a25d011260fdd6dfa1c447ed0
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-01-17 05:59:18 +0530

    Time: 79 ms (83.47%), Space: 59.5 MB (56.2%) - LeetHub

---
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long largestSquareArea(vector<std::vector<int>>& bottomLeft, vector<std::vector<int>>& topRight) {
+        int ans = 0, n = bottomLeft.size();
+        for (int i = 0; i < n; i++) {
+            vector<int>& firstRectBL = bottomLeft[i];
+            vector<int>& firstRectTR = topRight[i];
+            for (int j = i + 1; j < n; j++) {
+                vector<int>& secondRectBL = bottomLeft[j];
+                vector<int>& secondRectTR = topRight[j];
+                if (secondRectBL[0] >= firstRectTR[0] || secondRectTR[0] <= firstRectBL[0]) continue;
+                if (secondRectTR[1] <= firstRectBL[1] || secondRectBL[1] >= firstRectTR[1]) continue;
+                int pntAx = max(firstRectBL[0], secondRectBL[0]);
+                int pntAy = max(firstRectBL[1], secondRectBL[1]);
+                int pntBx = min(firstRectTR[0], secondRectTR[0]);
+                int pntBy = min(firstRectTR[1], secondRectTR[1]);
+                int sideA = pntBx - pntAx;
+                int sideB = pntBy - pntAy;
+                ans = max(ans, min(sideA, sideB));
+            }
+        }
+        return static_cast<long>(ans) * ans;
+    }
+};
