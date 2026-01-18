39d54d28644ceb839a2a3256266e5deb6874c358
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-01-18 16:52:51 +0530

    Time: 2450 ms (10.35%), Space: 19.4 MB (20.69%) - LeetHub

---
+++ 1895-largest-magic-square/1895-largest-magic-square.py
@@ -0,0 +1,30 @@
+class Solution:
+    def largestMagicSquare(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        res = 1
+
+        def isValid(i, j, k):
+            s = None
+            for x in range(i, i + k):
+                row = sum(grid[x][j:j + k])
+                if s is None: s = row
+                elif s != row: return False
+
+            for y in range(j, j + k):
+                if sum(grid[x][y] for x in range(i, i + k)) != s:
+                    return False
+
+            if sum(grid[i + d][j + d] for d in range(k)) != s:
+                return False
+
+            if sum(grid[i + d][j + k - 1 - d] for d in range(k)) != s:
+                return False
+
+            return True
+
+        for k in range(2, min(m, n) + 1):
+            for i in range(m - k + 1):
+                for j in range(n - k + 1):
+                    if isValid(i, j, k):
+                        res = k
+        return res
