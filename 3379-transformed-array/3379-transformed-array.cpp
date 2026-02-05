fec3033b0b62c5bc56067ba89bb6db49e335dee9
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-02-05 14:41:45 +0530

    Time: 10 ms (22.28%), Space: 25.5 MB (91.36%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& A) {
-        int n = A.size(), bias = n * (99 / n) + n;
-        vector<int> res;
+        int n = A.size();
+        int OFFSET = 1 << 7;
+        int MASK = (1 << 8) - 1;
+
         for (int i = 0; i < n; i++)
-            res.push_back(A[(i + A[i] + bias) % n]);
-        return res;
+            A[i] += OFFSET;
+
+        for (int i = 0; i < n; i++) {
+            int cur = A[i] - OFFSET;
+            int idx = ((i + cur) % n + n) % n;
+            int val = A[idx] & MASK;
+            A[i] |= val << 8;
+        }
+
+        for (int i = 0; i < n; i++)
+            A[i] = (A[i] >> 8) - OFFSET;
+
+        return A;
     }
 };
