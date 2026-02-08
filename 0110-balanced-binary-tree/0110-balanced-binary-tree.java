557de746e41cd0effbc09cc49be4207a94194cdf
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-02-08 16:48:13 +0530

    Time: 1 ms (30.78%), Space: 45.5 MB (75.66%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+
+    private int dfsHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = dfsHeight(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = dfsHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
