73f41c505b32ed06914f7ccf077d135e518766bb
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-02-24 17:07:41 +0530

    Time: 0 ms (100%), Space: 17.4 MB (53.22%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -12,12 +12,12 @@
 class Solution {
 public:
     int solve(TreeNode* root,int val){
-      if(!root)return 0;
-      val = 2*val + root->val;
-      if(!root->left && !root->right)return val;
-      return solve(root->left, val) + solve(root->right, val);
+        if(!root)return 0;
+        val = 2*val + root->val;
+        if(!root->left and !root->right)return val;
+        return solve(root->left, val) + solve(root->right,val); 
     }
     int sumRootToLeaf(TreeNode* root) {
-       return solve(root,0);
+        return solve(root,0);
     }
 };
