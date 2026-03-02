581a83136dd1c21b212e2f0905521d503d153de1
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-03-02 20:18:13 +0530

    Time: 5 ms (13.41%), Space: 29.3 MB (100%) - LeetHub

---
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; i++) {
+            int trail = 0;
+            for (int j = n - 1; j >= 0 && grid[i][j] == 0; j--)
+                trail++;
+            grid[i][0] = trail;
+        }
+        int swaps = 0;
+        for (int i = 0; i < n; i++) {
+            int j, req = n - 1 - i;
+            for (j = 0; j < grid.size(); j++)
+                if (grid[j][0] >= req) break;
+            if (j == grid.size()) return -1;
+            swaps += j;
+            grid.erase(grid.begin() + j);
+        }
+        return swaps;
+    }
+};
