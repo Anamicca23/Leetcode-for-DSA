71c56a2a97ee0908c2c734b1b9db97bf275b3a5a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-03-18 18:03:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int ans = 0;
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> px(m+1, vector<int>(n+1, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                px[i+1][j+1] = grid[i][j] + px[i][j+1] + px[i+1][j] - px[i][j];
-                if (px[i+1][j+1] <= k) ans++;
+        int n = grid.size(), m = grid[0].size(), ans = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (i > 0) grid[i][j] += grid[i - 1][j];
+                if (j > 0) grid[i][j] += grid[i][j - 1];
+                if (i > 0 && j > 0) grid[i][j] -= grid[i - 1][j - 1];
+
+                if (grid[i][j] <= k) ans++;
+                else break; 
             }
         }
         return ans;
