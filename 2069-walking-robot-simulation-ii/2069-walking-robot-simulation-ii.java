ff843905c2960e80733810b7b03a879e782c215e
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-04-07 05:50:38 +0530

    Time: 64 ms (69.93%), Space: 57.5 MB (84.62%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -1,41 +1,81 @@
 class Robot {
-    Map<Integer, Pair<int[], String>> map;
-    int currPos;
-    int perimeter;
-    boolean moved;
-    
-    public Robot(int width, int height) {
-        map = new HashMap<>();
-        currPos = 0;
-        perimeter = 0;
-        moved = false;
-        
-        map.put(0, new Pair(new int[]{0, 0}, "South"));
-        
-        for(int e = 1; e < width; ++e)
-            map.put(++perimeter, new Pair(new int[]{e, 0}, "East"));
-        
-        for(int n = 1; n < height; ++n)
-            map.put(++perimeter, new Pair(new int[]{width - 1, n}, "North"));
-        
-        for(int w = width - 2; w >= 0; --w)
-            map.put(++perimeter, new Pair(new int[]{w, height - 1}, "West"));
-        
-        for(int s = height - 2; s >= 0; --s)
-            map.put(++perimeter, new Pair(new int[]{0, s}, "South"));
-        
+    private final int width, height;
+    private int x, y;
+    private Direction dir;
+
+    public Robot(final int width, final int height) {
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+
+        final Direction east = new Direction("East", 1, 0);
+        final Direction north = new Direction("North", 0, 1);
+        final Direction west = new Direction("West", -1, 0);
+        final Direction south = new Direction("South", 0, -1);
+
+        east.next = north;
+        north.next = west;
+        west.next = south;
+        south.next = east;
+
+        this.dir = east;
     }
-    
-    public void move(int num) {
-        moved = true;
-        currPos = (currPos + num) % perimeter;
+
+    public void step(int num) {
+        final int perimeter = 2 * (width + height) - 4;
+
+        num = num % perimeter == 0 && num > 0 ? perimeter : num % perimeter;
+
+        while (num > 0) {
+            final int steps = stepsToEdge(dir.name, x, y, width, height);
+
+            if (num <= steps) {
+                x += dir.dx * num;
+                y += dir.dy * num;
+                num = 0;
+            } else {
+                x += dir.dx * steps;
+                y += dir.dy * steps;
+
+                num -= steps;
+                dir = dir.next;
+            }
+        }
     }
-    
+
     public int[] getPos() {
-        return map.get(currPos).getKey();
+        return new int[] { x, y };
     }
-    
+
     public String getDir() {
-        return moved ? map.get(currPos).getValue() : "East";
+        return dir.name;
+    }
+
+    private int stepsToEdge(final String name, final int x, final int y, final int width, final int height) {
+        switch (name) {
+            case "East":
+                return width - 1 - x;
+            case "North":
+                return height - 1 - y;
+            case "West":
+                return x;
+            case "South":
+                return y;
+            default:
+                return 0;
+        }
+    }
+
+    private final class Direction {
+        public final String name;
+        public final int dx, dy;
+        public Direction next;
+
+        public Direction(final String name, final int dx, final int dy) {
+            this.name = name;
+            this.dx = dx;
+            this.dy = dy;
+        }
     }
 }
