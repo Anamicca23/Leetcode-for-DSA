ed402b7928a038119e48451731ecd21dd6e84131
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-04-07 05:52:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -1,6 +1,6 @@
 #pragma GCC optimize("Ofast,unroll-loops,inline")
-#include <vector>
 #include <string>
+#include <vector>
 
 static const int speedup = []() {
     std::ios_base::sync_with_stdio(false);
@@ -17,25 +17,33 @@ public:
     Robot(int width, int height) : w(width), h(height), curr(0), moved(false) {
         p = (w + h - 2) << 1;
     }
-    
+
     inline void step(int num) {
         moved = true;
         curr = (curr + num) % p;
     }
-    
+
     inline std::vector<int> getPos() {
-        if (curr < w) return {curr, 0};
-        if (curr < w + h - 1) return {w - 1, curr - w + 1};
-        if (curr < (w << 1) + h - 2) return { (w << 1) + h - 3 - curr, h - 1};
+        if (curr < w)
+            return {curr, 0};
+        if (curr < w + h - 1)
+            return {w - 1, curr - w + 1};
+        if (curr < (w << 1) + h - 2)
+            return {(w << 1) + h - 3 - curr, h - 1};
         return {0, p - curr};
     }
-    
+
     inline std::string getDir() {
-        if (!moved) return dirs[0];
-        if (curr == 0) return dirs[3]; 
-        if (curr < w) return dirs[0];
-        if (curr < w + h - 1) return dirs[1];
-        if (curr < (w << 1) + h - 2) return dirs[2];
+        if (!moved)
+            return dirs[0];
+        if (curr == 0)
+            return dirs[3];
+        if (curr < w)
+            return dirs[0];
+        if (curr < w + h - 1)
+            return dirs[1];
+        if (curr < (w << 1) + h - 2)
+            return dirs[2];
         return dirs[3];
     }
 };
