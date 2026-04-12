da7b681a510548095f7023444f47567905343da4
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-04-12 05:45:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumDistance(String word) {
+        int dp[] = new int[26], res = 0, save = 0, n = word.length();
+        for (int i = 0; i < n - 1; ++i) {
+            int b = word.charAt(i) - 'A', c = word.charAt(i + 1) - 'A';
+            for (int a = 0; a < 26; ++a)
+                dp[b] = Math.max(dp[b], dp[a] + d(b, c) - d(a, c));
+            save = Math.max(save, dp[b]);
+            res += d(b, c);
+        }
+        return res - save;
+
+    }
+
+    private int d(int a, int b) {
+        return Math.abs(a / 6 - b / 6) + Math.abs(a % 6 - b % 6);
+    }
+}
