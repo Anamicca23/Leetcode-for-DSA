efca88abcdb6d0e9e28a2fae97f7480a434a8169
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-04-29 14:00:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.cpp
+++ 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.cpp
@@ -1,9 +1,10 @@
 class Solution {
-using ll = long long;
+    using ll = long long;
+
 public:
     long long maximumScore(vector<vector<int>>& grid) {
         int n = grid.size();
-        if (n == 1) 
+        if (n == 1)
             return 0;
         vector<ll> dp0(n + 1, 0), dp1(n + 1, 0);
 
@@ -12,7 +13,7 @@ public:
             for (int i = 0; i <= n; i++) {
                 ll prev = 0;
                 ll curr = 0;
-                for (int x = 0; x < i; x++) 
+                for (int x = 0; x < i; x++)
                     curr += grid[x][j];
                 for (int y = 0; y <= n; y++) {
                     if (y > 0 && y <= i) {
@@ -22,7 +23,8 @@ public:
                         prev += grid[y - 1][j - 1];
                     }
                     new_dp0[y] = max({new_dp0[y], prev + dp0[i], dp1[i]});
-                    new_dp1[y] = max({new_dp1[y], curr + dp1[i], curr + prev + dp0[i]});
+                    new_dp1[y] =
+                        max({new_dp1[y], curr + dp1[i], curr + prev + dp0[i]});
                 }
             }
             dp0 = new_dp0;
