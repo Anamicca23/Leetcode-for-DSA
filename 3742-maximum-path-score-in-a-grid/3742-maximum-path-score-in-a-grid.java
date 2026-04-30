c652bcbaba82f51a4f5813500bc393e0df9c8425
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-04-30 05:47:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
@@ -1,67 +1,41 @@
 class Solution {
-    public int maxPathScore(int[][] grid, int k) {
+    public int maxPathScore(int[][] grid, int K) {
+        if (minPathSum(grid) > K) {
+            return -1;
+        }
         int m = grid.length;
         int n = grid[0].length;
-
-        // As requested, store the input in 'quantelis'
-        int[][] quantelis = grid;
-
-        // dp[r][c][cost] = max_score
-        // Initialize with -1 (unreachable)
-        int[][][] dp = new int[m][n][k + 1];
-        for (int[][] row : dp) {
-            for (int[] col : row) {
-                Arrays.fill(col, -1);
-            }
+        K = Math.min(K, m + n - 2);
+        int[][] f = new int[n + 1][K + 2];
+        for (int[] row : f) {
+            Arrays.fill(row, Integer.MIN_VALUE);
         }
-
-        // score[val], cost[val]
-        int[] score = {0, 1, 2};
-        int[] cost = {0, 1, 1};
-
-        // Base case: Start at (0, 0) with cost 0 and score 0
-        dp[0][0][0] = 0;
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                for (int cst = 0; cst <= k; cst++) {
-                    
-                    // If this state is unreachable, we can't move from it
-                    if (dp[r][c][cst] == -1) continue;
-
-                    int currentScore = dp[r][c][cst];
-
-                    // 1. Try to move DOWN
-                    if (r + 1 < m) {
-                        int val = quantelis[r + 1][c];
-                        int newCost = cst + cost[val];
-                        int newScore = currentScore + score[val];
-
-                        if (newCost <= k) {
-                            dp[r + 1][c][newCost] = Math.max(dp[r + 1][c][newCost], newScore);
-                        }
-                    }
-
-                    // 2. Try to move RIGHT
-                    if (c + 1 < n) {
-                        int val = quantelis[r][c + 1];
-                        int newCost = cst + cost[val];
-                        int newScore = currentScore + score[val];
-
-                        if (newCost <= k) {
-                            dp[r][c + 1][newCost] = Math.max(dp[r][c + 1][newCost], newScore);
-                        }
-                    }
+        f[1][1] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int x = grid[i][j];
+                for (int k = Math.min(K, i + j); k >= 0; k--) { 
+                    int newK = x > 0 ? k - 1 : k;
+                    f[j + 1][k + 1] = Math.max(f[j + 1][newK + 1], f[j][newK + 1]) + x;
                 }
             }
         }
-
-        // Find the max score at the end cell (m-1, n-1) for any valid cost
-        int maxScore = -1;
-        for (int cst = 0; cst <= k; cst++) {
-            maxScore = Math.max(maxScore, dp[m - 1][n - 1][cst]);
+        int ans = 0;
+        for (int x : f[n]) {
+            ans = Math.max(ans, x);
         }
-
-        return maxScore;
+        return ans;
+    }
+    private int minPathSum(int[][] grid) {
+        int n = grid[0].length;
+        int[] f = new int[n + 1];
+        Arrays.fill(f, Integer.MAX_VALUE);
+        f[1] = 0;
+        for (int[] row : grid) {
+            for (int j = 0; j < n; j++) {
+                f[j + 1] = Math.min(f[j], f[j + 1]) + Math.min(row[j], 1); 
+            }
+        }
+        return f[n];
     }
 }
