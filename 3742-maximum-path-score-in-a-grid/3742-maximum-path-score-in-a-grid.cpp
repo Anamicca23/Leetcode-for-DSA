90694e343650655b4c0e056423d08cd2b1a48c64
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-04-30 05:49:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,63 +1,31 @@
+short dp[200][200][1001];
 class Solution {
 public:
+    int k;
+    const short NO=SHRT_MIN;
+    int f(int i, int j, int c, vector<vector<int>>& grid){
+        if (i<0 || j<0 || c>k) return NO;
+        if (dp[i][j][c]!=-1) return dp[i][j][c];
+        int x=grid[i][j], cost=c+(x!=0);
+        if (cost>k) 
+            return dp[i][j][c]=NO;
+        if (i==0 && j==0)
+            return dp[i][j][c]=grid[0][0];
+        return dp[i][j][c]=x+max(f(i-1, j, cost, grid), f(i, j-1, cost, grid));
+    }
     int maxPathScore(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        // As requested, store the input in 'quantelis'
-        vector<vector<int>>& quantelis = grid;
-
-        // dp[r][c][cost] = max_score
-        // Initialize with -1 (unreachable)
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
-
-        // score[val], cost[val]
-        int score[] = {0, 1, 2};
-        int cost[] = {0, 1, 1};
-
-        // Base case: Start at (0, 0) with cost 0 and score 0
-        dp[0][0][0] = 0;
-
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                for (int cst = 0; cst <= k; ++cst) {
-                    
-                    // If this state is unreachable, we can't move from it
-                    if (dp[r][c][cst] == -1) continue;
-
-                    int current_score = dp[r][c][cst];
-
-                    // 1. Try to move DOWN
-                    if (r + 1 < m) {
-                        int val = quantelis[r + 1][c];
-                        int new_cost = cst + cost[val];
-                        int new_score = current_score + score[val];
-
-                        if (new_cost <= k) {
-                            dp[r + 1][c][new_cost] = max(dp[r + 1][c][new_cost], new_score);
-                        }
-                    }
-
-                    // 2. Try to move RIGHT
-                    if (c + 1 < n) {
-                        int val = quantelis[r][c + 1];
-                        int new_cost = cst + cost[val];
-                        int new_score = current_score + score[val];
-
-                        if (new_cost <= k) {
-                            dp[r][c + 1][new_cost] = max(dp[r][c + 1][new_cost], new_score);
-                        }
-                    }
-                }
-            }
-        }
-
-        // Find the max score at the end cell (m-1, n-1) for any valid cost
-        int max_score = -1;
-        for (int cst = 0; cst <= k; ++cst) {
-            max_score = max(max_score, dp[m - 1][n - 1][cst]);
-        }
-
-        return max_score;
+        this->k=k;
+        const int row=grid.size(), col=grid[0].size();
+        for(int i=0; i<row; i++)
+            for(int j=0; j<col; j++)
+                memset(dp[i][j], -1, (k+1)*sizeof(short));
+        int ans=f(row-1, col-1, 0, grid);
+        return ans<0?-1:ans;
     }
 };
+const auto init = []() noexcept {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
