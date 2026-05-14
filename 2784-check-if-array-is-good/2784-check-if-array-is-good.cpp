e2a32a1c29544441a76fd388a7a7c09a586f474b
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-05-14 15:06:00 +0530

    Time: 0 ms (100%), Space: 26.4 MB (73.39%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size() - 1;
+        bitset<201> seen;
+        bool dup = 0;
+
+        for (auto& num : nums) {
+            if (num > n) return false;
+
+            if (seen.test(num)) {
+                if (num < n || dup) return false;
+                dup |= 1;
+                continue;
+            }
+
+            seen.set(num);
+        }
+
+        return true;
+    }
+};
