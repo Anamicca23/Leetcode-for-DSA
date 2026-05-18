838e00947b3fbd8967f0d994becb065d5255123a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-05-18 14:41:55 +0530

    Time: 54 ms (99.88%), Space: 80.1 MB (52.92%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,43 +1,150 @@
-constexpr int N = 5e4 + 1;
-bitset<N> vis;
-int q[N], front = 0, back = 0;
 class Solution {
 public:
-    static int minJumps(vector<int>& arr) {
-        const int n = arr.size();
-        vis.reset();
-        unordered_map<int, vector<int>> dict;
-        dict.reserve(n);
-        for (int i = 0; i < n; i++)
-            dict[arr[i]].push_back(i);
-
-        front = back = 0;
-        q[back++] = 0;
-        int step = 0;
-        while (front < back) {
-            int s = back - front;
-            while (s--) {
-                int cur = q[front++];
-                if (cur == n - 1)
-                    return step;
-                if (cur - 1 >= 0 && !vis[cur - 1]) {
-                    q[back++] = cur - 1;
-                    vis[cur - 1] = 1;
-                }
-                if (cur <= n && !vis[cur + 1]) {
-                    q[back++] = cur + 1;
-                    vis[cur + 1] = 1;
-                }
-                for (int idx : dict[arr[cur]]) {
-                    if (!vis[idx]) {
-                        q[back++] = idx;
-                        vis[idx] = 1;
-                    }
-                }
-                dict[arr[cur]].clear();
-            }
-            step++;
-        }
-        return -1;
+  int minJumps(std::vector<int> &arr) {
+    int last = arr.size() - 1;
+    if (arr.size() == 1)return 0;
+    if (arr.size() == 2 || arr[0] == arr[last]) return 1;
+    if (arr.size() == 3)return 2;
+    
+    std::unordered_map<int, std::vector<int>> nodes_by_value;
+    nodes_by_value.reserve(arr.size());
+    int max_repetitions = 1;
+    for (size_t i = 0; i < arr.size(); i++) {
+      auto &node = nodes_by_value[arr[i]];
+      node.push_back(i);
+      if (node.size() > max_repetitions) {
+        max_repetitions = node.size();
+      }
     }
+
+    if (max_repetitions == 1) {
+      return arr.size() - 1;
+    }
+
+    std::vector<int> depth(arr.size(), 0);
+    std::vector<int> depth_mirror(arr.size(), 0);
+    std::vector<int> deq;
+    deq.reserve(arr.size());
+    std::vector<int> deq_mirror;
+    deq_mirror.reserve(arr.size());
+    int deq_head = 0;
+    int deq_mirror_head = 0;
+    depth[0] = 1;
+    depth_mirror[last] = 1;
+
+    {
+
+      // enqueue right node
+      // not applicable yet
+      // if (depth_mirror[1] != 0) {
+      //   return depth[0] + depth_mirror[1] - 1;
+      // }
+
+      depth[1] = 2;
+      deq.push_back(1);
+
+      auto &connections = nodes_by_value[arr[0]];
+      for (auto &c : connections) {
+        if (depth[c] == 0) {
+          depth[c] = 2; // depth[0] + 1
+          deq.push_back(c);
+        }
+      }
+      connections.clear();
+
+      {
+        int left_mirror = last - 1;
+        if (depth[left_mirror] != 0) {
+          return depth_mirror[last] + depth[left_mirror] - 1;
+        }
+
+        depth_mirror[left_mirror] = 2;
+        deq_mirror.push_back(left_mirror);
+
+        auto &connections = nodes_by_value[arr[last]];
+        for (auto &c : connections) {
+          if (depth[c] != 0) {
+            return depth_mirror[last] + depth[c] - 1;
+          }
+
+          if (depth_mirror[c] == 0) {
+            depth_mirror[c] = 2; // depth_mirror[last] + 1
+            deq_mirror.push_back(c);
+          }
+        }
+        connections.clear();
+      }
+    }
+
+    while (true) {
+      auto index = deq[deq_head];
+      deq_head++;
+
+      // std::cout << "extracted index from deque: " << index << "\n";
+      if (depth_mirror[index] != 0) {
+        return depth[index] + depth_mirror[index] - 2;
+      }
+      // enqueue connected nodes
+      auto right = index + 1;
+      // check if the right index trace is found
+      // if (/*right < arr.size() && */ depth_mirror[right] != 0) {
+      //   return depth[index] + depth_mirror[right] - 1;
+      // }
+
+      if (/*right < arr.size() && */ depth[right] == 0) {
+        depth[right] = depth[index] + 1;
+        // std::cout << " DEQ " << "Adding right index: " << right << "\n";
+        deq.push_back(right);
+      }
+
+      auto left = index - 1;
+      // if (/*left >= 0 && */ depth_mirror[left] != 0) {
+      //   return depth[index] + depth_mirror[left] - 1;
+      // }
+
+      if ( depth[left] == 0) {
+        depth[left] = depth[index] + 1;
+        deq.push_back(left);
+      }
+
+      auto &connections = nodes_by_value[arr[index]];
+      for (auto &c : connections) {
+        if (depth[c] == 0) {
+          depth[c] = depth[index] + 1;
+          deq.push_back(c);
+        }
+      }
+      connections.clear();
+
+      {
+        auto index_mirror = deq_mirror[deq_mirror_head];
+        deq_mirror_head++;
+
+        if (depth[index_mirror] != 0) {
+          return depth_mirror[index_mirror] + depth[index_mirror] - 2;
+        }
+        auto right = index_mirror + 1;
+        if (depth_mirror[right] == 0) {
+          depth_mirror[right] = depth_mirror[index_mirror] + 1;
+          deq_mirror.push_back(right);
+        }
+
+        auto left = index_mirror - 1;
+        if (depth_mirror[left] == 0) {
+          depth_mirror[left] = depth_mirror[index_mirror] + 1;
+          deq_mirror.push_back(left);
+        }
+
+        auto &connections = nodes_by_value[arr[index_mirror]];
+        for (auto &c : connections) {
+          if (depth_mirror[c] == 0) {
+            depth_mirror[c] = depth_mirror[index_mirror] + 1;
+            deq_mirror.push_back(c);
+          }
+        }
+        connections.clear();
+      }
+    }
+    return depth[arr.size() - 1];
+  }
 };
