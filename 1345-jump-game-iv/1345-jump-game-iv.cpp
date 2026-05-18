0edb5c4347e468c72974a055e7e2c331f73a0bbf
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-05-18 14:39:07 +0530

    Time: 71 ms (89.14%), Space: 74.8 MB (97.55%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,43 @@
+constexpr int N = 5e4 + 1;
+bitset<N> vis;
+int q[N], front = 0, back = 0;
+class Solution {
+public:
+    static int minJumps(vector<int>& arr) {
+        const int n = arr.size();
+        vis.reset();
+        unordered_map<int, vector<int>> dict;
+        dict.reserve(n);
+        for (int i = 0; i < n; i++)
+            dict[arr[i]].push_back(i);
+
+        front = back = 0;
+        q[back++] = 0;
+        int step = 0;
+        while (front < back) {
+            int s = back - front;
+            while (s--) {
+                int cur = q[front++];
+                if (cur == n - 1)
+                    return step;
+                if (cur - 1 >= 0 && !vis[cur - 1]) {
+                    q[back++] = cur - 1;
+                    vis[cur - 1] = 1;
+                }
+                if (cur <= n && !vis[cur + 1]) {
+                    q[back++] = cur + 1;
+                    vis[cur + 1] = 1;
+                }
+                for (int idx : dict[arr[cur]]) {
+                    if (!vis[idx]) {
+                        q[back++] = idx;
+                        vis[idx] = 1;
+                    }
+                }
+                dict[arr[cur]].clear();
+            }
+            step++;
+        }
+        return -1;
+    }
+};
