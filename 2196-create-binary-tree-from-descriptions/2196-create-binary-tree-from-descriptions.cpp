ac1d2f0838365f3897c75630a549116c8530e95b
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-06-07 12:30:11 +0530

    Time: 181 ms (75.92%), Space: 279.1 MB (74.07%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,42 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, TreeNode*> map; // Map to store nodes by value
-        unordered_set<int> children; // Set to store child nodes
+    TreeNode* createBinaryTree(vector<vector<int>>& des) {
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> children;
 
-        // Iterate over descriptions to create nodes and link them
-        for (auto& arr : descriptions) {
-            int parent = arr[0], child = arr[1], isLeft = arr[2];
-            children.insert(child); // Add child to set of children
-
-            // Create parent node if it does not exist
-            if (map.find(parent) == map.end()) {
-                map[parent] = new TreeNode(parent);
+        for (auto& d : des) {
+            int p = d[0];
+            int c = d[1];
+            int isLeft = d[2];
+            if (mp.find(p) == mp.end()) {
+                mp[p] = new TreeNode(p);
             }
-
-            // Create child node if it does not exist
-            if (map.find(child) == map.end()) {
-                map[child] = new TreeNode(child);
+            if (mp.find(c) == mp.end()) {
+                mp[c] = new TreeNode(c);
             }
+            if (isLeft == 1)
+                mp[p]->left = mp[c];
+            else
+                mp[p]->right = mp[c];
+            children.insert(c);
+        }
 
-            // Link child to parent based on isLeft flag
-            if (isLeft == 1) {
-                map[parent]->left = map[child];
-            } else {
-                map[parent]->right = map[child];
+        for (auto& it : mp) {
+            if (children.find(it.first) == children.end()) {
+                return it.second;
             }
         }
 
-        int root = -1;
-        // Find the root node (node that is not a child of any other node)
-        for (auto& arr : descriptions) {
-            if (children.find(arr[0]) == children.end()) {
-                root = arr[0];
-                break;
-            }
-        }
-
-        // Return the root node
-        return map[root];
+        return nullptr;
     }
 };
