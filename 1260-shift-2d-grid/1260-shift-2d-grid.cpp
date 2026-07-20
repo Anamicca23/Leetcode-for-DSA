db1817e94b381914759d4bdf7a18aad679a02573
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-07-20 09:33:09 +0530

    Time: 4 ms (30.98%), Space: 18.2 MB (39.39%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        if (!k) return grid;
+        int r = grid.size(), c = grid[0].size();
+        int n = r * c;
+        k = k % n;
+        if (!k) return grid;
+        auto shift = [&](int i, int j) {
+            while (i < j) {
+                swap(grid[i / c][i % c], grid[j / c][j % c]);
+                i++;
+                j--;
+            }
+        };
+        shift(0, n - 1);
+        shift(0, k - 1);
+        shift(k, n - 1);
+        return grid;
+    }
+};
