7728bbf14671ce4e13ef61c3bf4f3c634a5baecf
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-07-31 19:36:08 +0530

    Time: 8 ms (61.48%), Space: 25.4 MB (84.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        vector<int> charCount(26, 0);
 
-        vector<int> hashMap(26,0);
-
-        for(auto &c: word){
-            hashMap[c-'a']++;
+        for(char ch : word) {
+            charCount[ch - 'a']++;
         }
 
-        sort(hashMap.begin(),hashMap.end(),greater<int>());
+        sort(charCount.begin(), charCount.end(), greater<int>());
 
-        int ans = 0;
-        int push = 1;
-        int keyCnt = 0;
+        int minPushCount = 0;
 
-        for(auto &freq: hashMap){
-
-            if(freq == 0) break;
-
-            ans += freq * push;
-            keyCnt++;
-
-            if(keyCnt % 8 == 0) push++;
-            
+        for(int i = 0; i < 26; i++) {
+            minPushCount += charCount[i] * (i / 8 + 1);
         }
 
-        return ans;
-        
+        return minPushCount;
     }
 };
