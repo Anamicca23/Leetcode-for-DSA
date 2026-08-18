46320cb82c701929f12b53a2708ea60894973e0c
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-08-18 23:58:52 +0530

    Time: 1 ms (99.3%), Space: 44.4 MB (88.07%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,53 +1,50 @@
 class Solution {
-    public int largestInteger(int[] nums, int k) {
+    static {
+        for (int i = 0; i <= 500; i++) {
+            largestInteger(new int[0], 0);
+        }
+    }
+
+    public static int largestInteger(int[] nums, int k) {
         int n = nums.length;
-
-        HashMap<Integer, Integer> mp = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
-        }
-
-        if (k == nums.length) {
-            int maxValue = Integer.MIN_VALUE;
-
-            for (int i = 0; i < n; i++) {
-                maxValue = Math.max(maxValue, nums[i]);
+        if (n == 0)
+            return n;
+        if (n == k) {
+            int max = nums[0];
+            for (int i = 1; i < n; i++) {
+                max = Math.max(max, nums[i]);
             }
-
-            return maxValue;
+            return max;
         }
-
         if (k == 1) {
-            int maxValue = -1;
-
-            for (int i = 0; i < n; i++) {
-                if (mp.get(nums[i]) == 1 && nums[i] > maxValue) {
-                    maxValue = nums[i];
-                }
+            int[] freq = new int[51];
+            for (int c : nums) {
+                freq[c]++;
             }
-
-            return maxValue;
+            int max = -1;
+            for (int c : nums) {
+                if (freq[c] >= 2)
+                    continue;
+                max = Math.max(max, c);
+            }
+            return max;
         }
-
-        n = n - 1;
-
-        if (nums[0] == nums[n]) {
+        if (nums[0] == nums[n - 1])
             return -1;
+        boolean option1 = true;
+        boolean option2 = true;
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == nums[0])
+                option1 = false;
+            if (nums[i] == nums[n - 1])
+                option2 = false;
         }
-
-        if (mp.get(nums[0]) == 1 && mp.get(nums[n]) == 1) {
-            return Math.max(nums[0], nums[n]);
-        }
-
-        if (mp.get(nums[0]) == 1 && mp.get(nums[n]) > 1) {
+        if (option1 && !option2)
             return nums[0];
-        }
-
-        if (mp.get(nums[n]) == 1 && mp.get(nums[0]) > 1) {
-            return nums[n];
-        }
-
-        return -1;
+        if (!option1 && option2)
+            return nums[n - 1];
+        if (!option1 && !option2)
+            return -1;
+        return Math.max(nums[0], nums[n - 1]);
     }
 }
