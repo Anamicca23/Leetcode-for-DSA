0e0405e0e20847f13f6a48422a57f80b2943c808
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-08-21 22:12:54 +0530

    Time: 6 ms (93.8%), Space: 21.2 MB (19.38%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -2,38 +2,63 @@ class Solution {
 public:
     using ll = long long;
     long long findKthSmallest(vector<int>& coins, int k) {
-        ranges::sort(coins);
-        vector<int> A;
-
-        for (auto& c : coins)
-            if (ranges::none_of(A, [&](int x) { return !(c % x); }))
-                A.push_back(c);
-
-        int n = A.size();
-
-        auto check = [&](ll mid) {
-            ll tot = 0;
-            for (int i = 1; i <= n; i++) {
-                int q = (1 << i) - 1;
-
-                while (q < 1 << n) {
-                    ll x = 1;
-                    for (int j = 0; j < n; j++)
-                        if ((q >> j) & 1)
-                            x = lcm(x, A[j]);
-
-                    tot += (mid / x) * (((i & 1) << 1) - 1);
-
-                    int c = q & -q;
-                    int r = q + c;
-                    q = (((r ^ q) >> 2) / c) | r;
+        sort(coins.begin(), coins.end());
+        vector<int> new_coins;
+        for (int x : coins) {
+            bool flag = true;
+            for (int y : new_coins) {
+                if (x % y == 0) {
+                    flag = false;
+                    break;
                 }
             }
-            return tot >= k;
+            if (flag) {
+                new_coins.push_back(x);
+            }
+        }
+        coins = new_coins;
+
+        int n = coins.size();
+        int m = (1 << n);
+        vector<int> bit_count(m);
+        vector<ll> lcm(m, 1);
+        ll l = k, r = 1ll * coins[0] * k + 1;
+
+        for (int mask = 1; mask < m; mask++) {
+            int pre_mask = mask & (mask - 1);
+            int i = __builtin_ctz(mask);
+
+            ll tmp = lcm[pre_mask] / gcd(lcm[pre_mask], coins[i]);
+            if (tmp <= r / coins[i]) {
+                lcm[mask] = tmp * coins[i];
+            } else {
+                lcm[mask] = r + 1;
+            }
+        }
+
+        auto get = [&](ll x) -> ll {
+            ll count = 0;
+            for (int mask = 1; mask < m; mask++) {
+                if (lcm[mask] > x) {
+                    continue;
+                }
+                if (__builtin_popcount(mask) & 1) {
+                    count += x / lcm[mask];
+                } else {
+                    count -= x / lcm[mask];
+                }
+            }
+            return count;
         };
 
-        ll low = k, high = 1ll * A[0] * k;
-        return *ranges::lower_bound(views::iota(low, high + 1), true, {},
-                                    [&](ll mid) { return check(mid); });
+        while (l < r) {
+            ll x = (l + r) >> 1;
+            if (get(x) >= k) {
+                r = x;
+            } else {
+                l = x + 1;
+            }
+        }
+        return l;
     }
 };
