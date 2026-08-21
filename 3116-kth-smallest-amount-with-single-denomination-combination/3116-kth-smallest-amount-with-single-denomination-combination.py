e777425e3af4a78ccb38b4ee0c41d9051bfde138
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-08-21 22:12:11 +0530

    Time: 6 ms (97.56%), Space: 19.4 MB (78.05%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
@@ -1,31 +1,41 @@
 class Solution:
-    def findKthSmallest(self, coins: list[int], k: int) -> int:
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
         coins.sort()
-        A = []
-        for c in coins:
-            if all(c % x for x in A):
-                A.append(c)
+        filtered_coins: List[int] = []
+        for index in range(len(coins)):
+            is_valid = True
+            for previous_index in range(index):
+                if coins[index] % coins[previous_index] == 0:
+                    is_valid = False
+                    break
+            if is_valid:
+                filtered_coins.append(coins[index])
+        smallest_possible = filtered_coins[0]
+        largest_possible = filtered_coins[0] * k
+        lcms_to_add: List[int] = []
+        lcms_to_substract: List[int] = []
+        for r in range(2, 16):
+            if r % 2 == 0:
+                for combo in combinations(filtered_coins, r):
+                    lcms_to_substract.append(lcm(*combo))
+            else:
+                for combo in combinations(filtered_coins, r):
+                    lcms_to_add.append(lcm(*combo))
 
-        n = len(A)
+        def is_possible(num: int) -> bool:
+            count_nums = 0
+            for coin in filtered_coins:
+                count_nums += num // coin
+            for lcm_to_add in lcms_to_add:
+                count_nums += num // lcm_to_add
+            for lcm_to_subtract in lcms_to_substract:
+                count_nums -= num // lcm_to_subtract
+            return count_nums >= k
 
-        def check(mid):
-            tot = 0
-            for i in range(1, n + 1):
-                q = (1 << i) - 1
-                lim = 1 << n
-                sgn = ((i & 1) << 1) - 1
-
-                while q < lim:
-                    x = 1
-                    for j in range(n):
-                        if (q >> j) & 1:
-                            x = lcm(x, A[j])
-
-                    tot += (mid // x) * sgn
-
-                    c = q & -q
-                    r = q + c
-                    q = (((r ^ q) >> 2) // c) | r
-            return tot >= k
-
-        return bisect_left(range(A[0] * k + 1), True, lo=k, key=check)
+        while largest_possible > smallest_possible:
+            mid = (smallest_possible + largest_possible) // 2
+            if is_possible(mid):
+                largest_possible = mid
+            else:
+                smallest_possible = mid + 1
+        return smallest_possible
