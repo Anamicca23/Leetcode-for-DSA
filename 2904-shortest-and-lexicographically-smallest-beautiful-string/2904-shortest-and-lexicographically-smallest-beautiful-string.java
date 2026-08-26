8c0cd8ea5435abbadb2ccf5fbfd5d8488994efe3
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-08-26 12:04:53 +0530

    Time: 1 ms (100%), Space: 43.9 MB (87.25%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,24 +1,32 @@
-class Solution {
+public class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        List<Integer> pos = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
-                pos.add(i);
+        char[] input = s.toCharArray();
+        int head = -1;
+        int tail = -1;
+        int back = 0;
+        int countOnes = 0;
+        for (int front = 0; front < input.length; ++front) {
+            countOnes += input[front] - '0';
+            if (countOnes < k) {
+                continue;
+            }
+            while (back < front && input[back] == '0') {
+                countOnes -= input[back] - '0';
+                ++back;
+            }
+            if (head == -1 || head - tail + 1 > front - back + 1) {
+                head = front;
+                tail = back;
+            } else if (head - tail + 1 == front - back + 1
+                    && s.substring(tail, head + 1).compareTo(s.substring(back, front + 1)) > 0) {
+                head = front;
+                tail = back;
+            }
+            while (back < front && countOnes == k) {
+                countOnes -= input[back] - '0';
+                ++back;
             }
         }
-        if (pos.size() < k)
-            return "";
-        String ans = "";
-        for (int i = 0; i + k - 1 < pos.size(); i++) {
-            int start = pos.get(i);
-            int end = pos.get(i + k - 1);
-            String cur = s.substring(start, end + 1);
-            if (ans.isEmpty() ||
-                cur.length() < ans.length() ||
-                (cur.length() == ans.length() && cur.compareTo(ans) < 0)) {
-                ans = cur;
-            }
-        }
-        return ans;
+        return head != -1 ? s.substring(tail, head + 1) : "";
     }
 }
